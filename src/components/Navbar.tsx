3f975b3ed9fc4a42ccfe162f2280dac0f39933d8
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-13 18:43:41 +0800

    add: update navbar logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
-import logo from "@/assets/logo.jpg";
+import logo from "@/assets/workifind-logo.svg";
 import { Button } from "@/components/ui/button";
 
 export default function Navbar() {
   return (
     <header className="shadow-sm">
-      <nav className="m-auto flex max-w-5xl items-center justify-between px-3 py-5">
+      <nav className="m-auto flex max-w-5xl items-center justify-between px-3 py-1">
         <Link href="/" className="flex items-center gap-3">
-          <Image src={logo} width={40} height={40} alt="Flow Jobs logo" />
-          <span className="text-xl font-bold tracking-wider">workifind</span>
+          <Image src={logo} width={70} height={70} alt="Flow Jobs logo" />
+          <span className="text-md font-mono font-semibold tracking-wider">workifind</span>
         </Link>
         <Button asChild>
           <Link href="/jobs/new">Post a job</Link>

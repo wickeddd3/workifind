4e15e29d423774eacff762ffec0c7d060f3365fb
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-14 18:04:08 +0800

    feat: add Footer and initial layout and design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={`${inter.className} min-w-[350px]`}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );

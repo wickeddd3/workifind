7b6da323598466e73577aaec6436e22d534339ca
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-16 01:04:26 +0800

    feat: add Navbar menu items

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,11 +6,35 @@ import { Button } from "@/components/ui/button";
 export default function Navbar() {
   return (
     <header className="shadow-sm">
-      <nav className="m-auto flex max-w-5xl items-center justify-between px-3 py-1">
+      <nav className="m-auto flex max-w-7xl items-center justify-between px-3 py-1">
         <Link href="/" className="flex items-center gap-3">
           <Image src={logo} width={70} height={70} alt="Flow Jobs logo" />
-          <span className="text-md font-mono font-semibold tracking-wider">workifind</span>
+          <span className="text-md font-mono font-semibold tracking-wider">
+            workifind
+          </span>
         </Link>
+        <div className="md:flex gap-6 hidden">
+          <Link href="/jobs" className="flex items-center gap-3">
+            <span className="text-sm tracking-wide">
+              Find jobs
+            </span>
+          </Link>
+          <Link href="/companies" className="flex items-center gap-3">
+            <span className="text-sm tracking-wide">
+              Companies
+            </span>
+          </Link>
+          <Link href="/professionals" className="flex items-center gap-3">
+            <span className="text-sm tracking-wide">
+              Professionals
+            </span>
+          </Link>
+          <Link href="/profile" className="flex items-center gap-3">
+            <span className="text-sm tracking-wide">
+              Profile
+            </span>
+          </Link>
+        </div>
         <Button asChild>
           <Link href="/jobs/new">Post a job</Link>
         </Button>

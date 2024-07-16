bedb459c3725130ec138ac4eaab766b51c474a44
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-17 01:50:22 +0800

    add: pass url query jobId to JobSelected component as props

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -2,13 +2,19 @@ import JobFilter from "@/components/jobs/JobFilter";
 import JobResults from "@/components/jobs/JobResults";
 import JobSelected from "@/components/jobs/JobSelected";
 
-export default function Page() {
+interface PageProps {
+  searchParams: {
+    jobId?: string;
+  };
+}
+
+export default function Page({ searchParams: { jobId } }: PageProps) {
   return (
     <main className="m-auto mb-10 space-y-6">
       <JobFilter />
       <section className="m-auto flex h-full max-w-7xl gap-4 px-3 md:flex-row">
         <JobResults />
-        <JobSelected />
+        <JobSelected jobId={jobId ? parseInt(jobId) : undefined} />
       </section>
     </main>
   );

467c11598988aef3295d9fb06e74153c8243448c
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-16 22:54:11 +0800

    feat: add nuxt config remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "xtg08hkzkg7wv2ci.public.blob.vercel-storage.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;

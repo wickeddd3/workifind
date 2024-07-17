640b52a486bbaa7105bd6e9829d2fafc2ef5fe99
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-17 23:50:11 +0800

    feat: get searchParams and pass as props defaultValues and filterValues

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,19 +1,33 @@
 import JobFilter from "@/components/jobs/JobFilter";
 import JobResults from "@/components/jobs/JobResults";
 import JobSelected from "@/components/jobs/JobSelected";
+import { JobFilterValues } from "@/lib/validation";
 
 interface PageProps {
   searchParams: {
+    q?: string;
+    type?: string;
+    salary?: string;
+    setup?: string;
     jobId?: string;
   };
 }
 
-export default function Page({ searchParams: { jobId } }: PageProps) {
+export default function Page({
+  searchParams: { q, type, salary, setup, jobId },
+}: PageProps) {
+  const filterValues: JobFilterValues = {
+    q,
+    type,
+    salary,
+    setup,
+  };
+
   return (
     <main className="m-auto mb-10 space-y-6">
-      <JobFilter />
+      <JobFilter defaultValues={filterValues} />
       <section className="m-auto flex h-full max-w-7xl gap-4 px-3 md:flex-row">
-        <JobResults />
+        <JobResults filterValues={filterValues} />
         <JobSelected jobId={jobId ? parseInt(jobId) : undefined} />
       </section>
     </main>

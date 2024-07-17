6c262b5ed2312ae40b85730ec3767e9a73264447
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-17 23:57:48 +0800

    feat: filter jobs using filter values props

--- src/components/jobs/JobResults.tsx
+++ src/components/jobs/JobResults.tsx
@@ -1,20 +1,65 @@
 import JobItem from "@/components/jobs/JobItem";
 import Link from "next/link";
 import prisma from "@/lib/prisma";
+import { JobFilterValues } from "@/lib/validation";
+import { Prisma } from "@prisma/client";
 
-export default async function JobResults() {
+interface JobResultsProps {
+  filterValues: JobFilterValues;
+}
+
+export default async function JobResults({ filterValues }: JobResultsProps) {
+  const { q, type, salary, setup } = filterValues;
+
+  const searchString = q
+    ?.split(" ")
+    .filter((word) => word.length > 0)
+    .join(" & ");
+
+  const searchFilter: Prisma.JobWhereInput = searchString
+    ? {
+        OR: [
+          { title: { search: searchString } },
+          { companyName: { search: searchString } },
+          { type: { search: searchString } },
+          { locationType: { search: searchString } },
+          { location: { search: searchString } },
+        ],
+      }
+    : {};
+
+  const where: Prisma.JobWhereInput = {
+    AND: [
+      searchFilter,
+      type ? { type } : {},
+      // location ? { location } : {},
+      setup ? { locationType: setup } : {},
+      { approved: true },
+    ],
+  };
+
   const jobs = await prisma.job.findMany({
-    where: {
-      approved: true,
-    },
+    where,
     orderBy: { createdAt: "desc" },
   });
+
+  function getLinkUrl(jobId: number): string {
+    const searchParams = new URLSearchParams({
+      ...(q && { q: q.trim() }),
+      ...(type && { type }),
+      ...(salary && { salary }),
+      ...(setup && { setup }),
+      ...(jobId && { jobId: jobId.toString() }),
+    });
+
+    return `/jobs?${searchParams.toString()}`;
+  }
 
   return (
     <div className="w-full space-y-4 md:w-2/5">
       {jobs.map((job) => (
         <Link
-          href={`/jobs?jobId=${job.id}`}
+          href={getLinkUrl(job.id)}
           scroll={false}
           key={job.id}
           className="block"

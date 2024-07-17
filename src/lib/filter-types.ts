08f4d341074b4fea3bd6144f1c8d768943e20ba6
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-17 23:49:02 +0800

    feat: add filter-types

---
+++ src/lib/filter-types.ts
@@ -0,0 +1,73 @@
+export const jobTypes = [
+  "Full-time",
+  "Part-time",
+  "Contract",
+  "Temporary",
+  "Internship",
+  "Volunteer",
+];
+
+export const jobSetup = ["Remote", "On-site", "Hybrid"];
+
+export const jobSalary = [
+  {
+    value: 10000,
+    name: "10k",
+  },
+  {
+    value: 20000,
+    name: "20k",
+  },
+  {
+    value: 30000,
+    name: "30k",
+  },
+  {
+    value: 40000,
+    name: "40k",
+  },
+  {
+    value: 50000,
+    name: "50k",
+  },
+  {
+    value: 60000,
+    name: "60k",
+  },
+  {
+    value: 70000,
+    name: "70k",
+  },
+  {
+    value: 80000,
+    name: "80k",
+  },
+  {
+    value: 90000,
+    name: "90k",
+  },
+  {
+    value: 100000,
+    name: "100k",
+  },
+  {
+    value: 110000,
+    name: "110k",
+  },
+  {
+    value: 120000,
+    name: "120k",
+  },
+  {
+    value: 130000,
+    name: "130k",
+  },
+  {
+    value: 140000,
+    name: "140k",
+  },
+  {
+    value: 150000,
+    name: "150k+",
+  },
+];

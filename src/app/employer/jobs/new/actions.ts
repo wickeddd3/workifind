15039f4a96640b82bc8443e440b242e362a9fdc6
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-21 00:48:32 +0800

    feat: update create new job action based on job schema changes

--- src/app/employer/jobs/new/actions.ts
+++ src/app/employer/jobs/new/actions.ts
@@ -3,55 +3,25 @@
 import { toSlug } from "@/lib/utils";
 import { createJobSchema } from "@/lib/validation";
 import { nanoid } from "nanoid";
-import { put } from "@vercel/blob";
-import path from "path";
 import prisma from "@/lib/prisma";
 import { redirect } from "next/navigation";
 
-export async function createJobPosting(formData: FormData) {
+export async function createJobPosting(id: number, formData: FormData) {
   const values = Object.fromEntries(formData.entries());
 
-  const {
-    title,
-    employmentType,
-    companyName,
-    companyLogo,
-    locationType,
-    location,
-    applicationEmail,
-    applicationUrl,
-    description,
-    salary,
-  } = createJobSchema.parse(values);
+  const { title, employmentType, locationType, location, description, salary } =
+    createJobSchema.parse(values);
 
   const slug = `${toSlug(title)}-${nanoid(10)}`;
 
-  let companyLogoUrl: string | undefined = undefined;
-
-  if (companyLogo) {
-    const blob = await put(
-      `company_logo/${slug}${path.extname(companyLogo.name)}`,
-      companyLogo,
-      {
-        access: "public",
-        addRandomSuffix: false,
-      },
-    );
-
-    companyLogoUrl = blob.url;
-  }
-
   await prisma.job.create({
     data: {
+      authorId: id,
       slug,
       title: title.trim(),
       employmentType,
-      companyName: companyName.trim(),
-      companyLogoUrl,
       locationType,
       location,
-      applicationEmail: applicationEmail?.trim(),
-      applicationUrl: applicationUrl?.trim(),
       description: description?.trim(),
       salary: parseInt(salary),
       approved: true,

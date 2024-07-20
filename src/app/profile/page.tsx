e59e24d4ada97cda0a475aae2b4ca8622e1cb2fa
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-21 00:05:39 +0800

    feat: fetch user employer profile if user role is employer and pass employer data as props in EmployerProfile component

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,29 +3,39 @@ import prisma from "@/lib/prisma";
 import ProfileSetup from "@/components/setup/ProfileSetup";
 import EmployerProfile from "@/app/profile/EmployerProfile";
 import ApplicantProfile from "@/app/profile/ApplicantProfile";
+import { cache } from "react";
 
 export default async function Page() {
   const { userId } = auth();
 
   const user = await prisma.user.findUnique({
     where: {
-      authId: userId || ''
+      authId: userId || "",
     },
     include: {
       employer: true,
       applicant: true,
-    }
+    },
   });
 
-  if(!user) {
-    return <ProfileSetup />
+  const getEmployerProfile = cache(async (userId: number | undefined) => {
+    if (!userId) return null;
+
+    return await prisma.employer.findUnique({
+      where: { userId },
+    });
+  });
+
+  if (!user) {
+    return <ProfileSetup />;
   }
 
-  if(user && user?.role === "EMPLOYER") {
-    return <EmployerProfile />
+  if (user && user?.role === "EMPLOYER") {
+    const employer = await getEmployerProfile(user.id);
+    return employer && <EmployerProfile employer={employer} />;
   }
 
-  if(user && user?.role === "APPLICANT") {
-    return <ApplicantProfile />
+  if (user && user?.role === "APPLICANT") {
+    return <ApplicantProfile />;
   }
 }

82a650cf540f56777350b3bf1ca1b1c5fbc3df50
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-21 00:48:14 +0800

    feat: fetch user before showing NewJobForm and pass userId as props

--- src/app/employer/jobs/new/page.tsx
+++ src/app/employer/jobs/new/page.tsx
@@ -1,10 +1,20 @@
 import { Metadata } from "next";
 import NewJobForm from "@/app/employer/jobs/new/NewJobForm";
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
 
 export const metadata: Metadata = {
   title: "Post a new job",
 };
 
-export default function Page() {
-  return <NewJobForm />
+export default async function Page() {
+  const { userId } = auth();
+
+  const user = await prisma.user.findUnique({
+    where: {
+      authId: userId || "",
+    },
+  });
+
+  return user && <NewJobForm userId={user.id} />;
 }

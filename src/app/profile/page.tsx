a036de0ff10428cafde33e188bec1a72bea83573
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-20 15:29:51 +0800

    feat: conditionally show ProfileSetup, EmployerProfile, and ApplicantProfile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,32 +1,31 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import ApplicantNewProfileForm from "@/components/applicant/ApplicantNewProfileForm";
-import EmployerNewProfileForm from "@/components/employer/EmployerNewProfileForm";
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
+import ProfileSetup from "@/components/setup/ProfileSetup";
+import EmployerProfile from "@/app/profile/EmployerProfile";
+import ApplicantProfile from "@/app/profile/ApplicantProfile";
 
-export default function Page() {
-  return (
-    <main className="m-auto my-10 max-w-3xl space-y-6">
-      <div className="space-y-1">
-        <h1 className="text-lg font-bold">Profile setup</h1>
-        <h2 className="text-md">Choose account type</h2>
-        <p className="text-sm font-light">
-          Choose how you want to use your acccount. Pick Employer or Applicant
-          profile.
-        </p>
-      </div>
-      <hr />
-      <Tabs defaultValue="applicant" className="w-full">
-        <TabsList className="w-full">
-          <TabsTrigger value="applicant" className="w-full uppercase font-bold tracking-wider">Applicant</TabsTrigger>
-          <span className="text-sm font-semibold tracking-tighter px-6">OR</span>
-          <TabsTrigger value="employer" className="w-full uppercase font-bold tracking-wider">Employer</TabsTrigger>
-        </TabsList>
-        <TabsContent value="applicant">
-          <ApplicantNewProfileForm />
-        </TabsContent>
-        <TabsContent value="employer">
-          <EmployerNewProfileForm />
-        </TabsContent>
-      </Tabs>
-    </main>
-  );
+export default async function Page() {
+  const { userId } = auth();
+
+  const user = await prisma.user.findUnique({
+    where: {
+      authId: userId || ''
+    },
+    include: {
+      employer: true,
+      applicant: true,
+    }
+  });
+
+  if(!user) {
+    return <ProfileSetup />
+  }
+
+  if(user && user?.role === "EMPLOYER") {
+    return <EmployerProfile />
+  }
+
+  if(user && user?.role === "APPLICANT") {
+    return <ApplicantProfile />
+  }
 }

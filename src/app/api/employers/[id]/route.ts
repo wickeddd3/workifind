e3417e4119a5ea427f61859c75fa9766550087af
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-24 02:56:24 +0800

    feat: add employers/id api route

---
+++ src/app/api/employers/[id]/route.ts
@@ -0,0 +1,29 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  const id = params.id;
+
+  try {
+    const employer = await prisma.employer.findUnique({
+      where: { userId: parseInt(id) },
+    });
+
+    if (!employer) {
+      return NextResponse.json(
+        { error: "Employer not found" },
+        { status: 404 },
+      );
+    }
+
+    return NextResponse.json({ employer }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error fetching employer data" },
+      { status: 500 },
+    );
+  }
+}

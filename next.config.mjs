58ab8323fc8143a5df11f231495b94a7a3a403b7
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-26 02:56:21 +0800

    chore: add NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  },
   images: {
     remotePatterns: [
       {

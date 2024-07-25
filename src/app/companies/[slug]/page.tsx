14d66a137ab0bafa933709d31edc89e9cf92a9f1
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-25 17:20:22 +0800

    feat: add company page initial layout and design

---
+++ src/app/companies/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import CompanyDetails from "@/components/companies/CompanyDetails";
+
+export default function Page() {
+  return (
+    <main className="m-auto mb-10 max-w-3xl space-y-6">
+      <CompanyDetails />
+    </main>
+  );
+}

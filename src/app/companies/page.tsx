65ffcbceddfcc8e99a1072eef854b26b7b02a720
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-25 21:52:43 +0800

    feat: fetch initial list of companies and pass response data as props to CompanyInitialList component

--- src/app/companies/page.tsx
+++ src/app/companies/page.tsx
@@ -1,12 +1,15 @@
+import { getInitialCompanies } from "@/actions/companies";
 import CompanyFilter from "@/components/companies/CompanyFilter";
 import CompanyInitialList from "@/components/companies/CompanyInitialList";
 import CompanySearchTip from "@/components/companies/CompanySearchTip";
 
-export default function Page() {
+export default async function Page() {
+  const companies = await getInitialCompanies();
+
   return (
     <main className="m-auto mb-10 max-w-5xl space-y-6">
       <CompanyFilter />
-      <CompanyInitialList />
+      {companies && <CompanyInitialList companies={companies} />}
       <CompanySearchTip />
     </main>
   );

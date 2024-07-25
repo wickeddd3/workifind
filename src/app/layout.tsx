3fc7b6f0a783572f7447b883b5c99ef1dc83a878
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-26 03:00:59 +0800

    feat: add publishableKey prop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
       <html lang="en">
         <body className={`${inter.className} min-w-[350px]`}>
           <UserProvider>

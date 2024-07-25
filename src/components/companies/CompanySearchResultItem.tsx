d682f958f1ac58bc4eba432e6d56de61b0c0c703
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-26 01:14:13 +0800

    feat: show company props details

--- src/components/companies/CompanySearchResultItem.tsx
+++ src/components/companies/CompanySearchResultItem.tsx
@@ -1,20 +1,27 @@
 import Image from "next/image";
 import imageLogoPlaceholder from "@/assets/workifind-logo.svg";
+import { Employer } from "@prisma/client";
 
-export default function CompanySearchResultItem() {
+interface CompanySearchResultItemProps {
+  company: Employer;
+}
+
+export default function CompanySearchResultItem({
+  company: { companyName, companyLogoUrl, industry, location },
+}: CompanySearchResultItemProps) {
   return (
     <article className="flex cursor-pointer items-center space-x-4 rounded-md border-2 border-gray-100 p-2 hover:bg-gray-50">
       <Image
-        src={imageLogoPlaceholder}
+        src={companyLogoUrl || imageLogoPlaceholder}
         width={120}
         height={100}
-        alt="Company logo"
+        alt={companyName}
       />
       <div className="flex flex-col">
-        <h3 className="text-lg font-medium text-gray-900">company name</h3>
-        <h4 className="text-md text-gray-700">industry</h4>
-        <h5 className="text-sm">location</h5>
-        <span className="flex w-fit rounded-xl bg-gray-300 px-2">
+        <h3 className="text-md font-medium text-gray-900">{companyName}</h3>
+        <h4 className="text-sm text-gray-700">{industry}</h4>
+        <h5 className="text-sm">{location}</h5>
+        <span className="mt-2 flex w-fit rounded-xl bg-gray-300 px-2">
           <span className="w-full p-1 text-xs font-semibold">8 jobs</span>
         </span>
       </div>

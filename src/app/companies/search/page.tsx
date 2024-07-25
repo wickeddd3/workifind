d99fc2a367b4e77045861836301560e6b4fd68e1
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-26 01:14:54 +0800

    feat: get searchParams and pass filterValues as props on CompanySearchFilter and CompanySearchResults

--- src/app/companies/search/page.tsx
+++ src/app/companies/search/page.tsx
@@ -1,11 +1,22 @@
 import CompanySearchFilter from "@/components/companies/CompanySearchFilter";
 import CompanySearchResults from "@/components/companies/CompanySearchResults";
+import { CompanyFilterValues } from "@/lib/validation";
 
-export default function Page() {
+interface PageProps {
+  searchParams: {
+    q?: string;
+  };
+}
+
+export default function Page({ searchParams: { q } }: PageProps) {
+  const filterValues: CompanyFilterValues = {
+    q,
+  };
+
   return (
     <main className="m-auto mb-10 max-w-2xl space-y-6">
-      <CompanySearchFilter />
-      <CompanySearchResults />
+      <CompanySearchFilter filterValues={filterValues} />
+      <CompanySearchResults filterValues={filterValues} />
     </main>
   );
 }

cee1e05b55861ceeb9a26386abe5e5756e142e83
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-26 01:13:12 +0800

    feat: redirect to /companies/search page with query params on filter and show query text

--- src/components/companies/CompanySearchFilter.tsx
+++ src/components/companies/CompanySearchFilter.tsx
@@ -1,14 +1,47 @@
-export default function CompanySearchFilter() {
+import { redirect } from "next/navigation";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { companyFilterSchema, CompanyFilterValues } from "@/lib/validation";
+
+async function searchCompanies(formData: FormData) {
+  "use server";
+
+  const values = Object.fromEntries(formData.entries());
+  const { q } = companyFilterSchema.parse(values);
+  const searchParams = new URLSearchParams({ ...(q && { q: q.trim() }) });
+
+  redirect(`/companies/search?${searchParams.toString()}`);
+}
+
+interface CompanySearchFilterProps {
+  filterValues: CompanyFilterValues;
+}
+
+export default function CompanySearchFilter({
+  filterValues,
+}: CompanySearchFilterProps) {
+  const { q } = filterValues;
+
   return (
     <section className="flex flex-col space-y-8 py-6">
       <h1 className="text-3xl font-medium">
-        Search results for &quot;accenture&ldquo;
+        Search results for &quot;{q}&ldquo;
       </h1>
-      <input
-        type="text"
-        placeholder="Search by company name"
-        className="rounded-lg border-2 border-gray-500 p-3"
-      />
+      <form
+        action={searchCompanies}
+        key="company-search-filter"
+        className="flex w-full gap-2"
+      >
+        <Input
+          id="q"
+          name="q"
+          type="text"
+          placeholder="Search by company name"
+          className="w-full"
+          defaultValue={q}
+        />
+        <Button>Search</Button>
+      </form>
     </section>
   );
 }

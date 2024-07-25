94dee014c82c78a0e229a263a99df8026b0f41b5
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-26 01:11:45 +0800

    feat: add searchCompanies action

--- src/actions/companies.ts
+++ src/actions/companies.ts
@@ -17,3 +17,22 @@ export async function getInitialCompanies(initialNumber?: number | string) {
 
   return null;
 }
+
+export async function searchCompanies(q: string) {
+  const params = {
+    q: q?.toString() || "",
+  };
+  const queryParams = new URLSearchParams(params).toString();
+
+  const response = await fetch(
+    `${baseUrl}/api/companies/search?${queryParams}`,
+  );
+
+  if (response.status === 200) {
+    const responseBody = await response.json();
+    const { companies } = responseBody;
+    return companies;
+  }
+
+  return null;
+}

80e2fa4720d6fc48e57a03185935317396b4b0cf
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-27 21:11:34 +0800

    feat: add home page initial layout and design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
-export default function Home() {
+import { getInitialCompanies } from "@/actions/companies";
+import CompanyInitialList from "@/components/companies/CompanyInitialList";
+import JobFilter from "@/components/jobs/JobFilter";
+import { JobFilterValues } from "@/lib/validation";
+
+export default async function Home() {
+  const filterValues: JobFilterValues = {
+    q: "",
+    employmentType: "",
+    salary: "",
+    locationType: "",
+  };
+
+  const companies = await getInitialCompanies();
+
   return (
-    <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
-      home
+    <main className="m-auto mb-10 space-y-6">
+      <JobFilter defaultValues={filterValues} />
+      <div className="m-auto flex h-full max-w-7xl flex-wrap items-center gap-4 px-3 md:flex-col">
+        <section className="h-[300px] w-full rounded-2xl bg-gray-50"></section>
+        {companies && <CompanyInitialList companies={companies} />}
+        <section className="flex flex-wrap items-center justify-between gap-10">
+          <div className="h-[300px] w-[600px] rounded-2xl bg-gray-50"></div>
+          <div className="h-[300px] w-[600px] rounded-2xl bg-gray-50"></div>
+        </section>
+      </div>
     </main>
   );
 }

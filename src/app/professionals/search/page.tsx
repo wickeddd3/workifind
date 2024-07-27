3ec25281d74cc6e52856828b7cc6c7be39294614
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-27 20:01:23 +0800

    feat: add professionals search page

---
+++ src/app/professionals/search/page.tsx
@@ -0,0 +1,22 @@
+import ProfessionalSearchFilter from "@/components/professionals/ProfessionalSearchFilter";
+import ProfessionalSearchResults from "@/components/professionals/ProfessionalSearchResults";
+import { ProfessionalFilterValues } from "@/lib/validation";
+
+interface PageProps {
+  searchParams: {
+    q?: string;
+  };
+}
+
+export default function Page({ searchParams: { q } }: PageProps) {
+  const filterValues: ProfessionalFilterValues = {
+    q,
+  };
+
+  return (
+    <main className="m-auto mb-10 max-w-2xl space-y-6">
+      <ProfessionalSearchFilter filterValues={filterValues} />
+      <ProfessionalSearchResults filterValues={filterValues} />
+    </main>
+  );
+}

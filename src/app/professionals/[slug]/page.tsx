1b5d4d84c3b57a0ffc2670bb848704be841d8295
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-27 20:01:30 +0800

    feat: add professional page

---
+++ src/app/professionals/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import ProfessionalDetails from "@/components/professionals/ProfessionalDetails";
+
+export default function Page() {
+  return (
+    <main className="m-auto mb-10 max-w-3xl space-y-6">
+      <ProfessionalDetails />
+    </main>
+  );
+}

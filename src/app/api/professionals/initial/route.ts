ab4e88028181cc7c88ac4072932588d089dc258e
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-27 19:59:53 +0800

    feat: add api route professional/initial

---
+++ src/app/api/professionals/initial/route.ts
@@ -0,0 +1,30 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+import { type NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const takeParam = searchParams.get("take") ?? "0";
+  const take = parseInt(takeParam) || 5;
+
+  try {
+    const professionals = await prisma.applicant.findMany({
+      orderBy: { createdAt: "desc" },
+      take,
+    });
+
+    if (!professionals) {
+      return NextResponse.json(
+        { error: "No professionals found" },
+        { status: 404 },
+      );
+    }
+
+    return NextResponse.json({ professionals }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error fetching initial professionals list data" },
+      { status: 500 },
+    );
+  }
+}

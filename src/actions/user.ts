d25ea36a255a0b83d0cd9fb00464be43be7e5561
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-29 01:25:13 +0800

    feat: add user actions

---
+++ src/actions/user.ts
@@ -0,0 +1,28 @@
+import { baseUrl } from "@/lib/baseUrl";
+
+export async function getUserById(id: number | string) {
+  const response = await fetch(`${baseUrl}/api/user`, {
+    method: "POST",
+    body: JSON.stringify({ userId: id }),
+  });
+
+  if (response.status === 200) {
+    const responseBody = await response.json();
+    const { user } = responseBody;
+    return user;
+  }
+
+  return null;
+}
+
+export async function getUser() {
+  const response = await fetch(`${baseUrl}/api/user`);
+
+  if (response.status === 200) {
+    const responseBody = await response.json();
+    const { user } = responseBody;
+    return user;
+  }
+
+  return null;
+}

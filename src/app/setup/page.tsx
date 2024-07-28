d2ef5e76a10884a5d2eb85030f5c338860890279
wickeddd3 <pablophilipandrew@gmail.com>
2024-07-29 01:26:59 +0800

    add: use getUser action instead of fetching user data using prisma directly

--- src/app/setup/page.tsx
+++ src/app/setup/page.tsx
@@ -1,19 +1,8 @@
-import { auth } from "@clerk/nextjs/server";
-import prisma from "@/lib/prisma";
 import ProfileSetup from "@/app/setup/ProfileSetup";
+import { getUser } from "@/actions/user";
 
 export default async function Page() {
-  const { userId } = auth();
-
-  const user = await prisma.user.findUnique({
-    where: {
-      authId: userId || "",
-    },
-    include: {
-      employer: true,
-      applicant: true,
-    },
-  });
+  const user = await getUser();
 
   if (!user) {
     return <ProfileSetup />;

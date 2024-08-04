8a013f438e3990ecc55c97b3139a28b9888df8d3
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-05 04:19:20 +0800

    feat: add createUser action

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -26,3 +26,18 @@ export async function getUser() {
 
   return null;
 }
+
+export async function createUser(userId: number | string, role: string) {
+  const response = await fetch(`${baseUrl}/api/user/create`, {
+    method: "POST",
+    body: JSON.stringify({ userId, role }),
+  });
+
+  if (response.status === 200) {
+    const responseBody = await response.json();
+    const { user } = responseBody;
+    return user;
+  }
+
+  return null;
+}

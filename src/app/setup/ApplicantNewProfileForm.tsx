26da36897f2ae8612d5f527aa54c36cbdea427ea
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-05 04:19:08 +0800

    feat: add new ApplicantNewProfileForm fields

--- src/app/setup/ApplicantNewProfileForm.tsx
+++ src/app/setup/ApplicantNewProfileForm.tsx
@@ -9,7 +9,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { useForm } from "react-hook-form";
+import { Controller, useFieldArray, useForm } from "react-hook-form";
 import { Label } from "@/components/ui/label";
 import RichTextEditor from "@/components/RichTextEditor";
 import { draftToMarkdown } from "markdown-draft-js";
@@ -19,11 +19,36 @@ import {
   CreateApplicantProfileValues,
 } from "@/lib/validation";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { createApplicantProfile } from "@/app/setup/actions";
+import { objectToFormData } from "@/lib/form-data";
+import { createApplicantProfile } from "@/actions/applicants";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { availability, employmentTypes, locationTypes } from "@/lib/job-types";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
+import { PlusIcon, XIcon } from "lucide-react";
 
 export default function ApplicantNewProfileForm() {
+  const defaultValues: CreateApplicantProfileValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    location: "",
+    about: "",
+    profession: "",
+    experienced: false,
+    skills: [],
+    languages: [],
+    availability: "",
+    preferredEmploymentTypes: [],
+    preferredLocationTypes: [],
+    preferredLocations: [],
+    salaryExpectation: 0,
+  };
+
   const form = useForm<CreateApplicantProfileValues>({
     resolver: zodResolver(createApplicantProfileSchema),
+    defaultValues,
   });
 
   const {
@@ -33,19 +58,36 @@ export default function ApplicantNewProfileForm() {
     formState: { isSubmitting },
   } = form;
 
-  async function onSubmit(values: CreateApplicantProfileValues) {
-    const formData = new FormData();
+  const {
+    fields: skillsFields,
+    append: skillsAppend,
+    remove: skillsRemove,
+  } = useFieldArray({
+    control: control,
+    name: "skills" as const,
+  });
 
-    Object.entries(values).forEach(([key, value]) => {
-      if (value) {
-        formData.append(key, value);
-      }
-    });
-    try {
-      await createApplicantProfile(formData);
-    } catch (error) {
-      alert("Something went wrong, please try again.");
-    }
+  const {
+    fields: languagesFields,
+    append: languagesAppend,
+    remove: languagesRemove,
+  } = useFieldArray({
+    control: control,
+    name: "languages" as const,
+  });
+
+  const {
+    fields: preferredLocationsFields,
+    append: preferredLocationsAppend,
+    remove: preferredLocationsRemove,
+  } = useFieldArray({
+    control: control,
+    name: "preferredLocations" as const,
+  });
+
+  async function onSubmit(values: CreateApplicantProfileValues) {
+    const formData = objectToFormData(values);
+    await createApplicantProfile(formData);
   }
 
   return (
@@ -70,7 +112,7 @@ export default function ApplicantNewProfileForm() {
                 <FormItem className="grow">
                   <FormLabel>First name</FormLabel>
                   <FormControl>
-                    <Input id="firstName" placeholder="e.g. John" {...field} />
+                    <Input id="firstName" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -83,7 +125,7 @@ export default function ApplicantNewProfileForm() {
                 <FormItem className="grow">
                   <FormLabel>Last name</FormLabel>
                   <FormControl>
-                    <Input id="lastName" placeholder="e.g. Doe" {...field} />
+                    <Input id="lastName" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -92,15 +134,14 @@ export default function ApplicantNewProfileForm() {
           </div>
           <FormField
             control={control}
-            name="email"
+            name="profession"
             render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
+              <FormItem className="grow">
+                <FormLabel>Profession</FormLabel>
                 <FormControl>
                   <Input
-                    id="email"
-                    placeholder="Email"
-                    type="email"
+                    id="profession"
+                    placeholder="e.g. Frontend Developer"
                     {...field}
                   />
                 </FormControl>
@@ -108,38 +149,319 @@ export default function ApplicantNewProfileForm() {
               </FormItem>
             )}
           />
-          <FormField
+          <div className="flex justify-between space-x-4">
+            <FormField
+              control={control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className="grow">
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input id="email" type="email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={control}
+              name="phoneNumber"
+              render={({ field }) => (
+                <FormItem className="grow">
+                  <FormLabel>Phone number</FormLabel>
+                  <FormControl>
+                    <Input id="phoneNumber" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex justify-between space-x-4">
+            <FormField
+              control={control}
+              name="location"
+              render={({ field }) => (
+                <FormItem className="grow">
+                  <FormLabel>Current Location</FormLabel>
+                  <FormControl>
+                    <Input id="location" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={control}
+              name="salaryExpectation"
+              render={({ field }) => (
+                <FormItem className="grow">
+                  <FormLabel>Salary expectation</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="number"
+                      onChange={(e) => field.onChange(Number(e.target.value))}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Controller
             control={control}
-            name="phoneNumber"
+            name="experienced"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Phone number</FormLabel>
-                <FormControl>
-                  <Input id="phoneNumber" {...field} />
+                <FormLabel>Work experience</FormLabel>
+                <FormControl className="py-2">
+                  <RadioGroup
+                    value={field.value.toString()}
+                    onValueChange={(value) => field.onChange(value === "true")}
+                    className="flex flex-wrap gap-6"
+                  >
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="false" id="noExperience" />
+                      <FormLabel htmlFor="noExperience" className="font-normal">
+                        No experience
+                      </FormLabel>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="true" id="withExperience" />
+                      <FormLabel
+                        htmlFor="withExperience"
+                        className="font-normal"
+                      >
+                        With experience
+                      </FormLabel>
+                    </div>
+                  </RadioGroup>
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
+          <div className="flex flex-col gap-3">
+            <FormLabel>Skills</FormLabel>
+            {skillsFields.map((field, index) => (
+              <FormItem
+                key={field.id}
+                className="flex flex-row items-center space-y-0"
+              >
+                <FormControl>
+                  <Controller
+                    control={control}
+                    name={`skills.${index}`}
+                    render={({ field }) => (
+                      <Input {...field} id={`skills-${index}`} />
+                    )}
+                  />
+                </FormControl>
+                <Button
+                  variant="link"
+                  size="icon"
+                  type="button"
+                  onClick={() => skillsRemove(index)}
+                >
+                  <XIcon size="16px" />
+                </Button>
+              </FormItem>
+            ))}
+            <div>
+              <Button
+                type="button"
+                variant="link"
+                size="sm"
+                className="flex items-center gap-2 px-0"
+                onClick={() => skillsAppend("")}
+              >
+                <PlusIcon size="16px" />
+                <span className="text-xs">Add skill</span>
+              </Button>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3">
+            <FormLabel>Languages</FormLabel>
+            {languagesFields.map((field, index) => (
+              <FormItem
+                key={field.id}
+                className="flex flex-row items-center space-y-0"
+              >
+                <FormControl>
+                  <Controller
+                    control={control}
+                    name={`languages.${index}`}
+                    render={({ field }) => (
+                      <Input {...field} id={`languages-${index}`} />
+                    )}
+                  />
+                </FormControl>
+                <Button
+                  variant="link"
+                  size="icon"
+                  type="button"
+                  onClick={() => languagesRemove(index)}
+                >
+                  <XIcon size="16px" />
+                </Button>
+              </FormItem>
+            ))}
+            <div>
+              <Button
+                type="button"
+                variant="link"
+                size="sm"
+                className="flex items-center gap-2 px-0"
+                onClick={() => languagesAppend("")}
+              >
+                <PlusIcon size="16px" />
+                <span className="text-xs">Add language</span>
+              </Button>
+            </div>
+          </div>
           <FormField
             control={control}
-            name="location"
+            name="availability"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Location</FormLabel>
-                <FormControl>
-                  <Input id="location" {...field} />
+                <FormLabel>Availability</FormLabel>
+                <FormControl className="py-2">
+                  <RadioGroup
+                    defaultValue=""
+                    value={field.value}
+                    onValueChange={(value) => field.onChange(value)}
+                    className="flex flex-wrap gap-6"
+                  >
+                    {availability.map((item) => (
+                      <div key={item} className="flex items-center space-x-2">
+                        <RadioGroupItem value={item} id={item} />
+                        <Label htmlFor={item} className="font-normal">
+                          {item}
+                        </Label>
+                      </div>
+                    ))}
+                  </RadioGroup>
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
+          <Controller
+            control={control}
+            name="preferredLocationTypes"
+            render={({ field }) => (
+              <FormItem>
+                <div className="mb-4">
+                  <FormLabel>Preferred location types</FormLabel>
+                </div>
+                <div className="flex flex-wrap gap-6">
+                  {locationTypes.map((item) => (
+                    <div
+                      key={item}
+                      className="flex flex-row items-start space-x-3 space-y-0"
+                    >
+                      <FormControl>
+                        <Checkbox
+                          checked={(field.value || []).includes(item)}
+                          onCheckedChange={(checked) => {
+                            const newValue = checked
+                              ? [...(field.value || []), item]
+                              : (field.value || []).filter(
+                                  (value) => value !== item,
+                                );
+                            field.onChange(newValue);
+                          }}
+                        />
+                      </FormControl>
+                      <FormLabel className="font-normal">{item}</FormLabel>
+                    </div>
+                  ))}
+                </div>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Controller
+            control={control}
+            name="preferredEmploymentTypes"
+            render={({ field }) => (
+              <FormItem>
+                <div className="mb-4">
+                  <FormLabel>Preferred employment types</FormLabel>
+                </div>
+                <div className="flex flex-wrap gap-6">
+                  {employmentTypes.map((item) => (
+                    <div
+                      key={item}
+                      className="flex flex-row items-start space-x-3 space-y-0"
+                    >
+                      <FormControl>
+                        <Checkbox
+                          checked={(field.value || []).includes(item)}
+                          onCheckedChange={(checked) => {
+                            const newValue = checked
+                              ? [...(field.value || []), item]
+                              : (field.value || []).filter(
+                                  (value) => value !== item,
+                                );
+                            field.onChange(newValue);
+                          }}
+                        />
+                      </FormControl>
+                      <FormLabel className="font-normal">{item}</FormLabel>
+                    </div>
+                  ))}
+                </div>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex flex-col gap-3">
+            <FormLabel>Preferred locations</FormLabel>
+            {preferredLocationsFields.map((field, index) => (
+              <FormItem
+                key={field.id}
+                className="flex flex-row items-center space-y-0"
+              >
+                <FormControl>
+                  <Controller
+                    control={control}
+                    name={`preferredLocations.${index}`}
+                    render={({ field }) => (
+                      <Input {...field} id={`preferredLocations-${index}`} />
+                    )}
+                  />
+                </FormControl>
+                <Button
+                  variant="link"
+                  size="icon"
+                  type="button"
+                  onClick={() => preferredLocationsRemove(index)}
+                >
+                  <XIcon size="16px" />
+                </Button>
+              </FormItem>
+            ))}
+            <div>
+              <Button
+                type="button"
+                variant="link"
+                size="sm"
+                className="flex items-center gap-2 px-0"
+                onClick={() => preferredLocationsAppend("")}
+              >
+                <PlusIcon size="16px" />
+                <span className="text-xs">Add location</span>
+              </Button>
+            </div>
+          </div>
           <FormField
             control={control}
             name="about"
             render={({ field: { value, onChange, ref } }) => (
               <FormItem>
-                <Label onClick={() => setFocus("about")}>About</Label>
+                <Label onClick={() => setFocus("about")}>About me</Label>
                 <FormControl>
                   <RichTextEditor
                     initialState={value}

435d02aee632ec9d7d28e5cae5cbcd73ebd717c4
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-10 08:11:20 +0800

    feat: add professionals/[id] api route for fetching professional details

---
+++ src/app/api/professionals/[id]/route.ts
@@ -0,0 +1,29 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  const id = params.id;
+
+  try {
+    const professional = await prisma.applicant.findUnique({
+      where: { id: parseInt(id) },
+    });
+
+    if (!professional) {
+      return NextResponse.json(
+        { error: "Professional not found" },
+        { status: 404 },
+      );
+    }
+
+    return NextResponse.json({ professional }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error fetching professional data" },
+      { status: 500 },
+    );
+  }
+}

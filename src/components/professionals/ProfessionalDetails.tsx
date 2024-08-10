98abe8cda2cd3e16db7929af4eb51cb3e5f591cf
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-10 08:17:28 +0800

    feat: show professional details with new design and layout

--- src/components/professionals/ProfessionalDetails.tsx
+++ src/components/professionals/ProfessionalDetails.tsx
@@ -1,55 +1,147 @@
-import imageLogoPlaceholder from "@/assets/workifind-logo.svg";
-import Image from "next/image";
 import Markdown from "../Markdown";
-import { Link as LinkIcon } from "lucide-react";
-import Link from "next/link";
+import { BadgeCheck, Briefcase, Mail, MapPin, Phone } from "lucide-react";
+import { Applicant } from "@prisma/client";
 
-export default function ProfessionalDetails() {
+interface ProfessionalDetailsProps {
+  professional: Applicant;
+}
+
+export default function ProfessionalDetails({
+  professional: {
+    firstName,
+    lastName,
+    email,
+    phoneNumber,
+    location,
+    about,
+    profession,
+    experienced,
+    skills,
+    languages,
+    availability,
+    preferredEmploymentTypes,
+    preferredLocationTypes,
+    preferredLocations,
+    salaryExpectation,
+  },
+}: ProfessionalDetailsProps) {
   return (
-    <section className="flex flex-col space-y-6">
-      {imageLogoPlaceholder && (
-        <Image
-          src={imageLogoPlaceholder}
-          alt="Company logo"
-          width={140}
-          height={140}
-          className="rounded-xl"
-        />
-      )}
-      <div className="flex flex-col space-y-2">
-        <h2 className="text-xl font-semibold">Accenture</h2>
-        <h6 className="flex items-center gap-3">
-          <LinkIcon size={16} className="shrink-0" />
-          <Link
-            href="http://www.google.com"
-            target="_blank"
-            className="text-sm font-normal hover:underline"
-          >
-            http://www.google.com
-          </Link>
-        </h6>
-      </div>
-      <div className="flex flex-col space-y-4">
-        <h1 className="text-2xl font-medium">Company Overview</h1>
-        <div className="flex items-center space-x-12">
-          <h3 className="text-md font-semibold">Industry</h3>
-          <p className="text-md">Information & technology</p>
+    <section className="flex flex-col space-y-6 pb-8">
+      <div className="flex flex-col space-y-2 rounded-xl bg-gray-50 p-8">
+        <div className="flex w-fit items-center justify-between gap-4">
+          <h2 className="text-2xl font-semibold">{`${firstName} ${lastName}`}</h2>
+          {experienced && <BadgeCheck size={16} className="shrink-0" />}
         </div>
-        <div className="flex items-center space-x-12">
-          <h3 className="text-md font-semibold">Location</h3>
-          <p className="text-md">makati philippines</p>
-        </div>
+        {profession && (
+          <p className="flex items-center gap-1.5">
+            <Briefcase size={16} className="shrink-0" />
+            <span className="text-sm">{profession}</span>
+          </p>
+        )}
+        {email && (
+          <p className="flex items-center gap-1.5">
+            <Mail size={16} className="shrink-0" />
+            <span className="text-sm">{email}</span>
+          </p>
+        )}
+        {location && (
+          <p className="flex items-center gap-1.5">
+            <MapPin size={16} className="shrink-0" />
+            <span className="text-sm">{location}</span>
+          </p>
+        )}
+        {phoneNumber && (
+          <p className="flex items-center gap-1.5">
+            <Phone size={16} className="shrink-0" />
+            <span className="text-sm">{phoneNumber}</span>
+          </p>
+        )}
       </div>
-      <div className="flex flex-col space-y-4">
-        <h1 className="text-xl font-medium">About us</h1>
-        <div className="text-md">
-          <Markdown>{"testttttttttttttt"}</Markdown>
-        </div>
-      </div>
-      <div className="flex flex-col space-y-4">
-        <h1 className="text-xl font-medium">Why join us?</h1>
-        <div className="text-md">
-          <Markdown>{"testttttttttttttt"}</Markdown>
+      <div className="flex flex-col gap-4 px-8">
+        {about && (
+          <div className="flex flex-col space-y-4">
+            <h1 className="text-lg font-medium">About me</h1>
+            <div className="text-md text-justify">
+              {about && <Markdown>{about}</Markdown>}
+            </div>
+          </div>
+        )}
+        {skills && skills.length > 0 && (
+          <div className="flex flex-col space-y-4">
+            <h1 className="text-lg font-medium">Skills</h1>
+            <div className="flex flex-wrap gap-2">
+              {skills.map((skill) => (
+                <span
+                  className="flex w-fit rounded-full bg-gray-100 px-2"
+                  key={skill}
+                >
+                  <span className="w-full p-2 text-sm font-semibold">
+                    {skill}
+                  </span>
+                </span>
+              ))}
+            </div>
+          </div>
+        )}
+        {languages && languages.length > 0 && (
+          <div className="flex flex-col space-y-4">
+            <h1 className="text-lg font-medium">Languages</h1>
+            <span className="text-sm">{languages.join(", ")}</span>
+          </div>
+        )}
+        <div className="flex flex-col space-y-4">
+          <h1 className="text-lg font-medium">About my next role</h1>
+          <div className="flex flex-col gap-2 rounded-xl border-2 border-gray-100 p-4">
+            {availability && (
+              <div className="flex flex-col gap-2">
+                <h1 className="text-md font-medium">Availability</h1>
+                <span className="text-sm">{availability}</span>
+              </div>
+            )}
+            {preferredEmploymentTypes &&
+              preferredEmploymentTypes.length > 0 && (
+                <div className="flex flex-col gap-2">
+                  <hr />
+                  <h1 className="text-md font-medium">
+                    Preferred employment types
+                  </h1>
+                  <span className="text-sm">
+                    {preferredEmploymentTypes.join(", ")}
+                  </span>
+                </div>
+              )}
+            {preferredLocationTypes && preferredLocationTypes.length > 0 && (
+              <div className="flex flex-col gap-2">
+                <hr />
+                <h1 className="text-md font-medium">
+                  Preferred location types
+                </h1>
+                <span className="text-sm">
+                  {preferredLocationTypes.join(", ")}
+                </span>
+              </div>
+            )}
+            {preferredLocations && preferredLocations.length > 0 && (
+              <div className="flex flex-col gap-2">
+                <hr />
+                <h1 className="text-md font-medium">
+                  Preferred location types
+                </h1>
+                {preferredLocations.map((location) => (
+                  <span className="text-sm" key={location}>
+                    {location}
+                  </span>
+                ))}
+              </div>
+            )}
+            {availability && (
+              <div className="flex flex-col gap-2">
+                <hr />
+                <h1 className="text-md font-medium">Salary expectation</h1>
+                <span className="text-sm">{salaryExpectation}</span>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </section>

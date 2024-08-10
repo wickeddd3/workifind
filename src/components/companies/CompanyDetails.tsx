4e12dca2997df6cdf4db2a5cfcfe7eaddfa079ce
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-10 22:48:01 +0800

    feat: show company details with new layout and design

--- src/components/companies/CompanyDetails.tsx
+++ src/components/companies/CompanyDetails.tsx
@@ -1,57 +1,132 @@
-import imageLogoPlaceholder from "@/assets/workifind-logo.svg";
 import Image from "next/image";
 import Markdown from "../Markdown";
-import { Link as LinkIcon } from "lucide-react";
+import { Link as LinkIcon, Mail } from "lucide-react";
 import Link from "next/link";
+import { Employer } from "@prisma/client";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-export default function CompanyDetails() {
+interface CompanyDetailsProps {
+  company: Employer;
+}
+
+export default function CompanyDetails({
+  company: {
+    companyName,
+    companyEmail,
+    companyWebsite,
+    companyLogoUrl,
+    industry,
+    location,
+    about,
+    pitch,
+    perks,
+  },
+}: CompanyDetailsProps) {
   return (
     <section className="flex flex-col space-y-6">
-      {imageLogoPlaceholder && (
+      {companyLogoUrl && (
         <Image
-          src={imageLogoPlaceholder}
-          alt="Company logo"
+          src={companyLogoUrl}
+          alt={companyName}
           width={140}
           height={140}
           className="rounded-xl"
         />
       )}
       <div className="flex flex-col space-y-2">
-        <h2 className="text-xl font-semibold">Accenture</h2>
-        <h6 className="flex items-center gap-3">
-          <LinkIcon size={16} className="shrink-0" />
-          <Link
-            href="http://www.google.com"
-            target="_blank"
-            className="text-sm font-normal hover:underline"
+        <h2 className="text-xl font-semibold">{companyName}</h2>
+        {companyWebsite && (
+          <h6 className="flex items-center gap-3">
+            <LinkIcon size={16} className="shrink-0" />
+            <Link
+              href={companyWebsite}
+              target="_blank"
+              className="text-sm font-normal hover:underline"
+            >
+              {companyWebsite}
+            </Link>
+          </h6>
+        )}
+        {companyEmail && (
+          <h6 className="flex items-center gap-3">
+            <Mail size={16} className="shrink-0" />
+            <Link
+              href={companyEmail}
+              target="_blank"
+              className="text-sm font-normal hover:underline"
+            >
+              {companyEmail}
+            </Link>
+          </h6>
+        )}
+      </div>
+      <Tabs defaultValue="about" className="w-full shadow-none">
+        <TabsList className="w-full justify-start rounded-none border-b-2 border-gray-200 bg-white p-0 shadow-none">
+          <TabsTrigger
+            value="about"
+            className="mr-8 rounded-none font-medium capitalize tracking-wider shadow-none data-[state=active]:font-bold data-[state=active]:text-[#3366FF] data-[state=active]:!shadow-none"
           >
-            http://www.google.com
-          </Link>
-        </h6>
-      </div>
-      <div className="flex flex-col space-y-4">
-        <h1 className="text-2xl font-medium">Company Overview</h1>
-        <div className="flex items-center space-x-12">
-          <h3 className="text-md font-semibold">Industry</h3>
-          <p className="text-md">Information & technology</p>
-        </div>
-        <div className="flex items-center space-x-12">
-          <h3 className="text-md font-semibold">Location</h3>
-          <p className="text-md">makati philippines</p>
-        </div>
-      </div>
-      <div className="flex flex-col space-y-4">
-        <h1 className="text-xl font-medium">About us</h1>
-        <div className="text-md">
-          <Markdown>{"testttttttttttttt"}</Markdown>
-        </div>
-      </div>
-      <div className="flex flex-col space-y-4">
-        <h1 className="text-xl font-medium">Why join us?</h1>
-        <div className="text-md">
-          <Markdown>{"testttttttttttttt"}</Markdown>
-        </div>
-      </div>
+            About
+          </TabsTrigger>
+          <TabsTrigger
+            value="culture"
+            className="rounded-none font-medium capitalize tracking-wider shadow-none data-[state=active]:font-bold data-[state=active]:text-[#3366FF] data-[state=active]:!shadow-none"
+          >
+            Life and culture
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="about" className="py-12">
+          <div className="flex flex-col space-y-8">
+            {(industry || location) && (
+              <div className="flex flex-col space-y-4">
+                <h1 className="text-lg font-medium">Company Overview</h1>
+                {industry && (
+                  <div className="flex items-center space-x-12">
+                    <h3 className="text-md font-semibold">Industry</h3>
+                    <p className="text-md">{industry}</p>
+                  </div>
+                )}
+                {location && (
+                  <div className="flex items-center space-x-12">
+                    <h3 className="text-md font-semibold">Location</h3>
+                    <p className="text-md">{location}</p>
+                  </div>
+                )}
+              </div>
+            )}
+            {about && (
+              <div className="flex flex-col space-y-4">
+                <h1 className="text-xl font-medium">About us</h1>
+                <div className="text-md">
+                  {about && <Markdown>{about}</Markdown>}
+                </div>
+              </div>
+            )}
+          </div>
+        </TabsContent>
+        <TabsContent value="culture" className="py-12">
+          <div className="flex flex-col space-y-8">
+            {pitch && (
+              <div className="flex flex-col space-y-4">
+                <h1 className="text-lg font-medium">Why join us?</h1>
+                <div className="text-md">
+                  {pitch && <Markdown>{pitch}</Markdown>}
+                </div>
+              </div>
+            )}
+            {perks && perks.length > 0 && (
+              <div className="flex flex-col space-y-4">
+                <h1 className="text-lg font-medium">Perks</h1>
+                <ul className="list-inside">
+                  {perks.map((item) => (
+                    <li key={item}>{item}</li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+        </TabsContent>
+      </Tabs>
     </section>
   );
 }

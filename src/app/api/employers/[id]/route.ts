ab4310356ee35707d70893089034aef84d00624f
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-11 23:00:19 +0800

    feat: add /employers/[id] update api route for updating employer profile

--- src/app/api/employers/[id]/route.ts
+++ src/app/api/employers/[id]/route.ts
@@ -27,3 +27,35 @@ export async function GET(
     );
   }
 }
+
+export async function PUT(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  const id = params.id;
+  const requestBody = await request.json();
+  const { form } = requestBody;
+
+  try {
+    const employer = await prisma.employer.update({
+      where: { id: parseInt(id) },
+      data: {
+        ...form,
+      },
+    });
+
+    if (!employer) {
+      return NextResponse.json(
+        { error: "Employer not found" },
+        { status: 404 },
+      );
+    }
+
+    return NextResponse.json({ employer }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error fetching employer data" },
+      { status: 500 },
+    );
+  }
+}

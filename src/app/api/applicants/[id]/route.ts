88ae789ced78018aca93a341a83c1735d1af117b
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-12 01:45:40 +0800

    feat: add /applicants/[id] api route for updating applicant profile

--- src/app/api/applicants/[id]/route.ts
+++ src/app/api/applicants/[id]/route.ts
@@ -27,3 +27,35 @@ export async function GET(
     );
   }
 }
+
+export async function PUT(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  const id = params.id;
+  const requestBody = await request.json();
+  const { form } = requestBody;
+
+  try {
+    const applicant = await prisma.applicant.update({
+      where: { id: parseInt(id) },
+      data: {
+        ...form,
+      },
+    });
+
+    if (!applicant) {
+      return NextResponse.json(
+        { error: "Applicant not found" },
+        { status: 404 },
+      );
+    }
+
+    return NextResponse.json({ applicant }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error fetching applicant data" },
+      { status: 500 },
+    );
+  }
+}

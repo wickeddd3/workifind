9072c6b623601cc904d9f6152af53e75a2179d6f
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-11 23:00:40 +0800

    feat: add updateEmployer and updateEmployerProfile actions

--- src/actions/employers.ts
+++ src/actions/employers.ts
@@ -47,6 +47,25 @@ export async function createEmployer(
   return null;
 }
 
+export async function updateEmployer(
+  id: number,
+  form: CreateEmployerProfileValues,
+) {
+  const response = await fetch(`${baseUrl}/api/employers/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({ form }),
+  });
+
+  if (response.status === 200) {
+    const responseBody = await response.json();
+    const { employer } = responseBody;
+
+    return employer;
+  }
+
+  return null;
+}
+
 export async function createEmployerProfile(
   formData: FormData,
 ): Promise<FormState> {
@@ -113,6 +132,78 @@ export async function createEmployerProfile(
     await createEmployer(user.id, form);
 
     // Step 9: Redirect to Profile Page
+    redirect("/employer/profile");
+  } catch (error) {
+    let message = "Unexpected error";
+    if (error instanceof Error) {
+      message = error.message;
+    }
+    return { error: message };
+  }
+}
+
+export async function updateEmployerProfile(
+  id: number,
+  formData: FormData,
+): Promise<FormState> {
+  try {
+    // Step 1: Verify if Employer ID exist
+    if (!id) return { error: "Employer id missing" };
+
+    // Step 2: Transform Form Data
+    const rawData = Object.fromEntries(formData.entries());
+    const transformedData = {
+      ...rawData,
+      perks:
+        typeof rawData.perks === "string" && rawData.perks
+          ? rawData.perks.split(",").map((s) => s.trim())
+          : [],
+    };
+
+    // Step 3: Validate Data
+    const parsedData = createEmployerProfileSchema.safeParse(transformedData);
+    if (!parsedData.success) {
+      console.error("Validation Errors:", parsedData.error.format());
+      return { error: "Validation failed" };
+    }
+    const validatedData = parsedData.data;
+
+    // Step 4: Create a slug based on companyName
+    const slug = `${toSlug(validatedData.companyName)}-${nanoid(10)}`;
+
+    // Step 5: Upload Company Logo if exist
+    let companyLogoUrl: string | undefined = undefined;
+    const { companyLogo } = validatedData;
+    if (companyLogo) {
+      const blob = await put(
+        `company_logo/${slug}${path.extname(companyLogo.name)}`,
+        companyLogo,
+        {
+          access: "public",
+          addRandomSuffix: false,
+        },
+      );
+      companyLogoUrl = blob.url;
+    }
+
+    // Step 6: Prepare Form Data
+    const form = {
+      slug,
+      companyLogoUrl,
+      companyName: validatedData.companyName?.trim(),
+      companyEmail: validatedData.companyEmail?.trim(),
+      companyWebsite: validatedData.companyWebsite?.trim(),
+      location: validatedData.location?.trim(),
+      about: validatedData.about?.trim(),
+      pitch: validatedData.pitch?.trim(),
+      perks: validatedData.perks,
+      industry: validatedData.industry,
+    };
+
+    // Step 7: Update Employer Profile
+    await updateEmployer(id, form);
+
+    // Step 8: Redirect to Profile Page
     redirect("/employer/profile");
   } catch (error) {
     let message = "Unexpected error";

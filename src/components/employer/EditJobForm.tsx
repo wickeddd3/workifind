c63f43fad97cca5d5031ce620c247c27a7cc8048
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-13 19:03:07 +0800

    feat: add EditJobForm component

---
+++ src/components/employer/EditJobForm.tsx
@@ -0,0 +1,241 @@
+"use client";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Select from "@/components/ui/select";
+import { createJobSchema, CreateJobValues } from "@/lib/validation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { employmentTypes, locationTypes } from "@/lib/job-types";
+import { Label } from "@/components/ui/label";
+import RichTextEditor from "@/components/RichTextEditor";
+import { draftToMarkdown } from "markdown-draft-js";
+import LoadingButton from "@/components/LoadingButton";
+import { objectToFormData } from "@/lib/form-data";
+import { updateJobPost } from "@/actions/jobs";
+import { Job } from "@prisma/client";
+
+interface NewJobFormProps {
+  userId: number;
+  slug: string;
+  job: Job;
+}
+
+export default function EditJobForm({
+  userId,
+  slug,
+  job: {
+    title,
+    employmentType,
+    description,
+    salaryStart,
+    salaryEnd,
+    location,
+    locationType,
+  },
+}: NewJobFormProps) {
+  const defaultValues: CreateJobValues = {
+    title,
+    employmentType,
+    locationType,
+    description: description ?? "",
+    salaryStart: salaryStart ?? 0,
+    salaryEnd: salaryEnd ?? 0,
+    location: location ?? "",
+  };
+
+  const form = useForm<CreateJobValues>({
+    resolver: zodResolver(createJobSchema),
+    defaultValues,
+  });
+
+  const {
+    handleSubmit,
+    trigger,
+    control,
+    setFocus,
+    formState: { isSubmitting },
+  } = form;
+
+  async function onSubmit(values: CreateJobValues) {
+    const formData = objectToFormData(values);
+    await updateJobPost(userId, slug, formData);
+  }
+
+  return (
+    <main className="m-auto space-y-6 px-4">
+      <div>
+        <h1 className="text-md font-semibold">{`Edit ${title} job post`}</h1>
+        <p className="text-sm text-muted-foreground">
+          Get your job posting seen by thousands of job seekers.
+        </p>
+      </div>
+      <div className="space-y-6 rounded-lg border p-4">
+        <div>
+          <h2 className="font-semibold">Job details</h2>
+          <p className="text-muted-foreground">
+            Provide a job description and details
+          </p>
+        </div>
+        <Form {...form}>
+          <form
+            className="space-y-4"
+            noValidate
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <FormField
+              control={control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Job title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="e.g. Frontend Developer" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={control}
+              name="employmentType"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Employment type</FormLabel>
+                  <FormControl>
+                    <Select {...field}>
+                      <option value="" hidden>
+                        Select an option
+                      </option>
+                      {employmentTypes.map((type) => (
+                        <option key={type} value={type}>
+                          {type}
+                        </option>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={control}
+              name="locationType"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Work setup</FormLabel>
+                  <FormControl>
+                    <Select
+                      {...field}
+                      onChange={(e) => {
+                        field.onChange(e);
+                        if (e.currentTarget.value === "Remote") {
+                          trigger("location");
+                        }
+                      }}
+                    >
+                      <option value="" hidden>
+                        Select an option
+                      </option>
+                      {locationTypes.map((locationType) => (
+                        <option key={locationType} value={locationType}>
+                          {locationType}
+                        </option>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={control}
+              name="location"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Office location</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={control}
+              name="description"
+              render={({ field: { value, onChange, ref } }) => (
+                <FormItem>
+                  <Label onClick={() => setFocus("description")}>
+                    Description
+                  </Label>
+                  <FormControl>
+                    <RichTextEditor
+                      initialState={value}
+                      onChange={(draft) => onChange(draftToMarkdown(draft))}
+                      ref={ref}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex flex-col space-y-4">
+              <FormLabel>Salary range</FormLabel>
+              <div className="flex justify-between space-x-4">
+                <FormField
+                  control={control}
+                  name="salaryStart"
+                  render={({ field }) => (
+                    <FormItem className="grow">
+                      <FormLabel>Minimum salary</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="number"
+                          {...field}
+                          onChange={(e) =>
+                            field.onChange(Number(e.target.value))
+                          }
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={control}
+                  name="salaryEnd"
+                  render={({ field }) => (
+                    <FormItem className="grow">
+                      <FormLabel>Maximum salary</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="number"
+                          {...field}
+                          onChange={(e) =>
+                            field.onChange(Number(e.target.value))
+                          }
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            <LoadingButton type="submit" loading={isSubmitting}>
+              Submit
+            </LoadingButton>
+          </form>
+        </Form>
+      </div>
+    </main>
+  );
+}

4adf0b540676a24f40d341bed75a759c0293c1ff
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-13 08:33:16 +0800

    feat: fetch employer jobs and pass jobs data to EmployerJobs as props

--- src/app/employer/jobs/page.tsx
+++ src/app/employer/jobs/page.tsx
@@ -1,3 +1,24 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+import { useUser } from "@/contexts/UserContext";
+import { getEmployerJobs } from "@/actions/jobs";
+import EmployerJobs from "@/components/employer/EmployerJobs";
+
 export default function Page() {
-  return <h1>list of jobs created</h1>
+  const { user } = useUser();
+  const [jobs, setJobs] = useState(null);
+
+  const handleGetJobs = useCallback(
+    async (id: number) => await getEmployerJobs(id),
+    [],
+  );
+
+  useEffect(() => {
+    if (user && user?.id) {
+      handleGetJobs(user.id).then(setJobs);
+    }
+  }, [user, handleGetJobs]);
+
+  return jobs && <EmployerJobs jobs={jobs} />;
 }

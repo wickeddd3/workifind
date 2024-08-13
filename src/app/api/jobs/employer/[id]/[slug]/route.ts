553162a85c35a63db7539533ab1254d4a31500ca
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-13 18:55:01 +0800

    feat: add jobs/employer/[id]/[slug] DELETE api route for deleting job based on authorId and job slug

--- src/app/api/jobs/employer/[id]/[slug]/route.ts
+++ src/app/api/jobs/employer/[id]/[slug]/route.ts
@@ -56,3 +56,27 @@ export async function PUT(
   }
 }
 
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string; slug: string } },
+) {
+  const id = params.id;
+  const slug = params.slug;
+
+  try {
+    const job = await prisma.job.delete({
+      where: { authorId: parseInt(id), slug },
+    });
+
+    if (!job) {
+      return NextResponse.json({ error: "Job not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ job }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error fetching job data" },
+      { status: 500 },
+    );
+  }
+}

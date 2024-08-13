8a3a7e343ba54da9eea67b1f39c1b615e34d934d
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-13 18:54:39 +0800

    feat: add jobs/employer/[id]/[slug] PUT api route for updating job based on authorId and job slug

--- src/app/api/jobs/employer/[id]/[slug]/route.ts
+++ src/app/api/jobs/employer/[id]/[slug]/route.ts
@@ -26,3 +26,33 @@ export async function GET(
   }
 }
 
+export async function PUT(
+  request: Request,
+  { params }: { params: { id: string; slug: string } },
+) {
+  const id = params.id;
+  const slug = params.slug;
+  const requestBody = await request.json();
+  const { form } = requestBody;
+
+  try {
+    const job = await prisma.job.update({
+      where: { authorId: parseInt(id), slug },
+      data: {
+        ...form,
+      },
+    });
+
+    if (!job) {
+      return NextResponse.json({ error: "Job not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ job }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error fetching job data" },
+      { status: 500 },
+    );
+  }
+}
+

b2a3ac1c1f7d309215c7a15e8d2ecad508361091
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-13 08:33:35 +0800

    feat: add EmployerJobs component

---
+++ src/components/employer/EmployerJobs.tsx
@@ -0,0 +1,19 @@
+import { Job } from "@prisma/client";
+import EmployerJobItem from "@/components/employer/EmployerJobItem";
+
+interface EmployerJobsProps {
+  jobs: Job[];
+}
+
+export default function EmployerJobs({ jobs }: EmployerJobsProps) {
+  return (
+    <main className="m-auto space-y-6 px-4">
+      <h1 className="px-4 text-xl font-medium">My jobs</h1>
+      <div className="flex flex-col gap-2">
+        {jobs.map((job) => (
+          <EmployerJobItem job={job} key={job.id} />
+        ))}
+      </div>
+    </main>
+  );
+}

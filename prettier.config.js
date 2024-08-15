5fe83c2a8f46bb063219963b3ac769217a1fc7f6
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-15 13:36:27 +0800

    chore: update eslint and prettier configs

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   plugins: ["prettier-plugin-tailwindcss"],
+  tabWidth: 2,
+  semi: true,
+  trailingComma: "all",
+  bracketSpacing: true,
+  printWidth: 80,
+  endOfLine: "auto",
 };

6385464dcaf412a73c65ec224ad8365d884260de
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-15 18:24:06 +0800

    add: HomeSearchHistory, HomeInitialSavedJobs, and HomeInitialCompanyList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { getInitialCompanies } from "@/actions/companies";
-import CompanyInitialList from "@/components/companies/CompanyInitialList";
+import HomeInitialCompanyList from "@/components/home/HomeInitialCompanyList";
+import HomeInitialSavedJobs from "@/components/home/HomeInitialSavedJobs";
 import HomeMarketingSection from "@/components/home/HomeMarketingSection";
+import HomeSearchHistory from "@/components/home/HomeSearchHistory";
 import JobFilter from "@/components/jobs/JobFilter";
 import { JobFilterValues } from "@/lib/validation";
 
-export default async function Home() {
+export default function Page() {
   const filterValues: JobFilterValues = {
     q: "",
     employmentType: "",
@@ -12,14 +13,13 @@ export default async function Home() {
     locationType: "",
   };
 
-  const companies = await getInitialCompanies();
-
   return (
     <main className="m-auto mb-10 space-y-6">
       <JobFilter defaultValues={filterValues} />
-      <div className="m-auto flex h-full max-w-7xl flex-wrap items-center gap-4 px-3 md:flex-col">
-        <section className="h-[300px] w-full rounded-2xl bg-gray-50"></section>
-        {companies && <CompanyInitialList companies={companies} />}
+      <div className="m-auto flex h-full max-w-7xl flex-wrap items-center gap-6 px-3 md:flex-col">
+        <HomeSearchHistory />
+        <HomeInitialSavedJobs />
+        <HomeInitialCompanyList />
         <HomeMarketingSection />
       </div>
     </main>

16679d72de773a89578b176184f45c58e1f2a2fe
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-17 03:55:09 +0800

    feat: add jobs/[slug] api route for fetching job details using slug

---
+++ src/app/api/jobs/[slug]/route.ts
@@ -0,0 +1,29 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { slug: string } },
+) {
+  const slug = params.slug;
+
+  try {
+    const job = await prisma.job.findUnique({
+      where: { slug },
+      include: {
+        employer: true,
+      },
+    });
+
+    if (!job) {
+      return NextResponse.json({ error: "Job not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ job }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error fetching job data" },
+      { status: 500 },
+    );
+  }
+}

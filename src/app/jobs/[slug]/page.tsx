e813a780931aa6d3944fb6b48634475ae9fe0c9d
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-17 03:57:50 +0800

    feat: use getJob action instead of fetching job details directly to prisma

--- src/app/jobs/[slug]/page.tsx
+++ src/app/jobs/[slug]/page.tsx
@@ -1,19 +1,14 @@
 import { cache } from "react";
-import prisma from "@/lib/prisma";
 import { notFound } from "next/navigation";
 import JobDetails from "@/components/jobs/JobDetails";
+import { getJob } from "@/actions/jobs";
 
 interface PageProps {
   params: { slug: string };
 }
 
-const getJob = cache(async (slug: string) => {
-  const job = await prisma.job.findUnique({
-    where: { slug },
-    include: {
-      employer: true,
-    },
-  });
+const handleGetJob = cache(async (slug: string) => {
+  const job = await getJob(slug);
 
   if (!job) notFound();
 
@@ -21,7 +16,7 @@ const getJob = cache(async (slug: string) => {
 });
 
 export default async function Page({ params: { slug } }: PageProps) {
-  const job = await getJob(slug);
+  const job = await handleGetJob(slug);
 
   return (
     <main className="mx-auto max-w-4xl p-4">

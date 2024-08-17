0b171b2ae3329121e055504e2b89e6b0a65015fe
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-18 04:17:25 +0800

    feat: add ApplicantJobApplicationItem component

---
+++ src/components/applicant/ApplicantJobApplicationItem.tsx
@@ -0,0 +1,84 @@
+import { Job, JobApplication } from "@prisma/client";
+import {
+  Banknote,
+  Briefcase,
+  EllipsisVertical,
+  MapPin,
+  Trash,
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { formatMoney } from "@/lib/utils";
+
+interface ApplicantJobApplicationItemProps {
+  jobApplication: JobApplication & { job: Job };
+}
+
+export default function ApplicantJobApplicationItem({
+  jobApplication: {
+    job: { title, employmentType, locationType },
+    job,
+  },
+}: ApplicantJobApplicationItemProps) {
+  const salary = (job: Job) => {
+    const { minSalary, maxSalary } = job;
+    if (!minSalary && !maxSalary) {
+      return null;
+    }
+    if (minSalary === maxSalary) {
+      return formatMoney(minSalary);
+    }
+    return `${formatMoney(minSalary)} - ${formatMoney(maxSalary)}`;
+  };
+
+  return (
+    <div className="flex flex-col gap-2 rounded-lg bg-gray-50 px-4 py-2 hover:bg-gray-100">
+      <div className="flex items-center justify-between">
+        <h4 className="text-md font-medium capitalize text-gray-900">
+          {title}
+        </h4>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon" className="h-8 w-8">
+              <EllipsisVertical size={16} className="shrink-0" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56" align="end">
+            <DropdownMenuGroup>
+              <DropdownMenuItem className="cursor-pointer">
+                <Trash className="mr-2 h-4 w-4" />
+                <span>Delete</span>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="flex flex-col">
+        {employmentType && (
+          <p className="flex items-center gap-1.5 text-sm text-muted-foreground">
+            <Briefcase size={16} className="shrink-0" />
+            {employmentType}
+          </p>
+        )}
+        {locationType && (
+          <p className="flex items-center gap-1.5 text-sm text-muted-foreground">
+            <MapPin size={16} className="shrink-0" />
+            {locationType}
+          </p>
+        )}
+        {salary(job) && (
+          <p className="flex items-center gap-1.5 text-sm text-muted-foreground">
+            <Banknote size={16} className="shrink-0" />
+            {salary(job)}
+          </p>
+        )}
+      </div>
+    </div>
+  );
+}

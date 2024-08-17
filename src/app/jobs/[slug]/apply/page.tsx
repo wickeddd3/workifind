c5f2d5a827c921772daa1e2ca08d24f958ce9f82
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-17 16:26:01 +0800

    feat: add job apply page with job application validation to check if applicant already applied to the selected job, if not proceed to show application form otherwise will be redirected to job submitted page

--- src/app/jobs/[slug]/apply/page.tsx
+++ src/app/jobs/[slug]/apply/page.tsx
@@ -1,27 +1,68 @@
+"use client";
+
+import { getApplicant } from "@/actions/applicants";
 import { getJob } from "@/actions/jobs";
 import JobApplicationForm from "@/components/jobs/JobApplicationForm";
-import { notFound } from "next/navigation";
-import { cache } from "react";
+import { useUser } from "@/contexts/UserContext";
+import { Applicant, Job, JobApplication } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
 
 interface PageProps {
   params: { slug: string };
 }
 
-const handleGetJob = cache(async (slug: string) => {
-  const job = await getJob(slug);
+export default function Page({ params: { slug } }: PageProps) {
+  const router = useRouter();
+  const { user } = useUser();
+  const [job, setJob] = useState(null);
+  const [hasApplication, setHasApplication] = useState(true);
 
-  if (!job) notFound();
+  const handleGetJobAndApplicant = useCallback(
+    async (jobSlug: string, userId: number) => {
+      return await Promise.all([getJob(jobSlug), getApplicant(userId)]);
+    },
+    [],
+  );
 
-  return job;
-});
+  const handleCheckIfApplicantHasApplication = (
+    job: Job & { jobApplications: JobApplication[] },
+    applicant: Applicant,
+  ) => {
+    const { jobApplications } = job;
+    const { id } = applicant;
+    return jobApplications.some(
+      (application: JobApplication) => application.applicantId === id,
+    );
+  };
 
-export default async function Page({ params: { slug } }: PageProps) {
-  const job = await handleGetJob(slug);
+  const handleJobApplicationValidation = useCallback(
+    async (jobSlug: string, userId: number) => {
+      const [job, applicant] = await handleGetJobAndApplicant(jobSlug, userId);
+      setJob(job);
+      const hasApplication = handleCheckIfApplicantHasApplication(
+        job,
+        applicant,
+      );
+      if (hasApplication) {
+        router.push(`/jobs/${slug}/submitted`);
+      } else {
+        setHasApplication(false);
+      }
+    },
+    [router, slug, handleGetJobAndApplicant],
+  );
+
+  useEffect(() => {
+    if (user && user?.id) {
+      handleJobApplicationValidation(slug, user.id);
+    }
+  }, [slug, user, handleJobApplicationValidation]);
 
   return (
     <main className="mx-auto max-w-4xl p-4">
       <div className="h-full">
-        <JobApplicationForm job={job} />
+        {job && !hasApplication && <JobApplicationForm job={job} />}
       </div>
     </main>
   );

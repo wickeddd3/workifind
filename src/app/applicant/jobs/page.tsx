a2df94c755dbd910ceea0498b5cc9796c380e4f4
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-18 04:17:40 +0800

    feat: list applicant job applications

--- src/app/applicant/jobs/page.tsx
+++ src/app/applicant/jobs/page.tsx
@@ -1,3 +1,48 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+import { useUser } from "@/contexts/UserContext";
+import ApplicantJobApplications from "@/components/applicant/ApplicantJobApplications";
+import { getApplicantJobApplications } from "@/actions/jobApplications";
+import { getApplicant } from "@/actions/applicants";
+
 export default function Page() {
-  return <h1>list of jobs applied</h1>;
+  const { user } = useUser();
+  const [applicant, setApplicant] = useState(null);
+  const [jobApplications, setJobApplications] = useState(null);
+
+  const handleGetApplicant = useCallback(
+    async (id: number) => await getApplicant(id),
+    [],
+  );
+
+  const handleGetJobApplications = useCallback(
+    async (id: number) => await getApplicantJobApplications(id),
+    [],
+  );
+
+  const handleGetApplicantJobApplications = useCallback(
+    async (userId: number) => {
+      const applicant = await handleGetApplicant(userId);
+      setApplicant(applicant);
+      if (applicant) {
+        const jobApplications = await handleGetJobApplications(applicant.id);
+        setJobApplications(jobApplications);
+      }
+    },
+    [handleGetApplicant, handleGetJobApplications],
+  );
+
+  useEffect(() => {
+    if (user && user?.id) {
+      handleGetApplicantJobApplications(user.id);
+    }
+  }, [user, handleGetApplicantJobApplications]);
+
+  return (
+    applicant &&
+    jobApplications && (
+      <ApplicantJobApplications jobApplications={jobApplications} />
+    )
+  );
 }

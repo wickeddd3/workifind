a778a811391c3e66fabbf947dcf06cc5b4ba068f
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-20 03:04:48 +0800

    feat: add optional excluded array param

--- src/lib/form-data.ts
+++ src/lib/form-data.ts
@@ -1,10 +1,12 @@
-export function objectToFormData(values: object) {
+// excluded array is a list of excluded property to be converted to string
+export function objectToFormData(values: object, excluded?: string[]) {
   const formData = new FormData();
 
   if (!(typeof values === "object")) return formData;
 
   Object.entries(values).forEach(([key, value]) => {
-    if (typeof value === "object") {
+    const isExcluded = (excluded || []).some((item) => item === key);
+    if (typeof value === "object" && !isExcluded) {
       formData.append(key, JSON.stringify(value));
     } else if (value) {
       formData.append(key, value);

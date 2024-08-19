fbc975ec52030c6e40f5146b3341b2827393a391
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-20 03:05:44 +0800

    feat: update usage of perks array

--- src/components/setup/EmployerNewProfileForm.tsx
+++ src/components/setup/EmployerNewProfileForm.tsx
@@ -65,7 +65,7 @@ export default function EmployerNewProfileForm() {
   });
 
   async function onSubmit(values: CreateEmployerProfileValues) {
-    const formData = objectToFormData(values);
+    const formData = objectToFormData(values, ["companyLogo"]);
     const createdEmployerProfile = await createEmployerProfile(formData);
     if (createdEmployerProfile) {
       router.push("/employer/profile");
@@ -209,7 +209,7 @@ export default function EmployerNewProfileForm() {
                 <FormControl>
                   <Controller
                     control={control}
-                    name={`perks.${index}`}
+                    name={`perks.${index}.name`}
                     render={({ field }) => (
                       <Input {...field} id={`perks-${index}`} />
                     )}
@@ -231,7 +231,7 @@ export default function EmployerNewProfileForm() {
                 variant="link"
                 size="sm"
                 className="flex items-center gap-2 px-0"
-                onClick={() => perksAppend("")}
+                onClick={() => perksAppend({ name: "" })}
               >
                 <PlusIcon size="16px" />
                 <span className="text-xs">Add perks</span>

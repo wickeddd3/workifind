2a6dcc911d0db25ef43ca7e3dc861e0a61a6a526
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-23 04:08:15 +0800

    fix: RichTextEditor issue of rerendering

--- src/components/RichTextEditor.tsx
+++ src/components/RichTextEditor.tsx
@@ -2,8 +2,8 @@
 
 import { cn } from "@/lib/utils";
 import dynamic from "next/dynamic";
-import { forwardRef } from "react";
-import { EditorState, ContentState } from "draft-js";
+import { forwardRef, useState } from "react";
+import { EditorState, ContentState, convertToRaw } from "draft-js";
 import { EditorProps } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
@@ -16,36 +16,50 @@ interface RichTextEditorProps extends EditorProps {
   initialState?: string;
 }
 
-export default forwardRef<unknown, RichTextEditorProps>(
-  function RichTextEditor(props, ref) {
-    const editorState = props.initialState
-      ? EditorState.createWithContent(
-          ContentState.createFromText(props.initialState),
-        )
-      : EditorState.createEmpty();
+export default forwardRef<unknown, RichTextEditorProps>(function RichTextEditor(
+  { initialState, ...props },
+  ref,
+) {
+  // Manage the editor state
+  const [editorState, setEditorState] = useState<EditorState>(() => {
+    if (initialState) {
+      const contentState = ContentState.createFromText(initialState);
+      return EditorState.createWithContent(contentState);
+    }
+    return EditorState.createEmpty();
+  });
 
-    return (
-      <Editor
-        editorState={editorState}
-        editorClassName={cn(
-          "border rounded-md px-3 min-h-[150px] cursor-text ring-offset-background focus-within:outline-none focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2",
-          props.editorClassName,
-        )}
-        toolbar={{
-          options: ["inline", "list", "link", "history"],
-          inline: {
-            options: ["bold", "italic", "underline"],
-          },
-        }}
-        editorRef={(r) => {
-          if (typeof ref === "function") {
-            ref(r);
-          } else if (ref) {
-            ref.current = r;
-          }
-        }}
-        {...props}
-      />
-    );
-  },
-);
+  // Handle editor state change
+  const handleEditorStateChange = (state: EditorState) => {
+    setEditorState(state);
+    if (props.onChange) {
+      const contentState = state.getCurrentContent();
+      props.onChange(convertToRaw(contentState));
+    }
+  };
+
+  return (
+    <Editor
+      editorState={editorState}
+      onEditorStateChange={handleEditorStateChange}
+      editorClassName={cn(
+        "border rounded-md px-3 min-h-[150px] cursor-text ring-offset-background focus-within:outline-none focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2",
+        props.editorClassName,
+      )}
+      toolbar={{
+        options: ["inline", "list", "link", "history"],
+        inline: {
+          options: ["bold", "italic", "underline"],
+        },
+      }}
+      editorRef={(r) => {
+        if (typeof ref === "function") {
+          ref(r);
+        } else if (ref) {
+          ref.current = r;
+        }
+      }}
+      {...props}
+    />
+  );
+});

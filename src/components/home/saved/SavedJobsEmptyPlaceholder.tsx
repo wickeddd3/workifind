d011a5bf8ca763d4d4d90cfbce1e3ba166705c87
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-24 03:43:18 +0800

    feat: add SavedJobsEmptyPlaceholder component

---
+++ src/components/home/saved/SavedJobsEmptyPlaceholder.tsx
@@ -0,0 +1,19 @@
+import IllustrationJobSaved from "@/components/illustrations/IllustrationJobSaved";
+
+interface SavedJobsEmptyPlaceholderProps {
+  message?: string;
+}
+
+export default function SavedJobsEmptyPlaceholder({
+  message = "Start saving jobs that match your preferences",
+}: SavedJobsEmptyPlaceholderProps) {
+  return (
+    <div className="flex flex-col items-center justify-center gap-2 rounded-2xl bg-gray-50 px-4 py-8">
+      <IllustrationJobSaved />
+      <h4 className="text-lg font-semibold">No saved jobs</h4>
+      <p className="text-md text-center font-normal text-muted-foreground">
+        {message}
+      </p>
+    </div>
+  );
+}

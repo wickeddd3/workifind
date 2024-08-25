95a2602b1b7f3ba06160becd4537b26b15c6321f
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-26 03:34:56 +0800

    feat: pass page url query param in CompanySearchResults as page prop

--- src/app/companies/search/page.tsx
+++ src/app/companies/search/page.tsx
@@ -5,10 +5,11 @@ import { CompanyFilterValues } from "@/lib/validation";
 interface PageProps {
   searchParams: {
     q?: string;
+    page?: string;
   };
 }
 
-export default function Page({ searchParams: { q } }: PageProps) {
+export default function Page({ searchParams: { q, page } }: PageProps) {
   const filterValues: CompanyFilterValues = {
     q,
   };
@@ -16,7 +17,10 @@ export default function Page({ searchParams: { q } }: PageProps) {
   return (
     <main className="m-auto mb-10 max-w-2xl space-y-6">
       <CompanySearchFilter filterValues={filterValues} />
-      <CompanySearchResults filterValues={filterValues} />
+      <CompanySearchResults
+        filterValues={filterValues}
+        page={page ? parseInt(page) : undefined}
+      />
     </main>
   );
 }

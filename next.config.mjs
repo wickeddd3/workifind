46ebe475def66941d16fbb506483df014a112474
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-26 15:23:05 +0800

    add: crossOrigin anonymous in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  crossOrigin: "anonymous",
   env: {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,

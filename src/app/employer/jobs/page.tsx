89e6cb4265c21fc22d9bdfeee4fad299acc25ca2
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-27 03:27:01 +0800

    add: employer jobs empty placeholder

--- src/app/employer/jobs/page.tsx
+++ src/app/employer/jobs/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useUser } from "@/contexts/UserContext";
 import { getEmployerJobs } from "@/actions/jobs";
 import EmployerJobs from "@/components/employer/EmployerJobs";
+import { Job, JobApplication } from "@prisma/client";
+import EmployerJobsEmptyPlaceholder from "@/components/employer/EmployerJobsEmptyPlaceholder";
 
 export default function Page() {
   const { user } = useUser();
-  const [jobs, setJobs] = useState(null);
+  const [jobs, setJobs] = useState<
+    (Job & { jobApplications: JobApplication[] })[] | null
+  >(null);
+
+  const hasEmptyJobs = useMemo(() => jobs && jobs.length === 0, [jobs]);
 
   const handleGetJobs = useCallback(
     async (id: number) => await getEmployerJobs(id),
@@ -20,5 +26,10 @@ export default function Page() {
     }
   }, [user, handleGetJobs]);
 
-  return jobs && <EmployerJobs jobs={jobs} />;
+  return (
+    <>
+      {jobs && <EmployerJobs jobs={jobs} />}
+      {hasEmptyJobs && <EmployerJobsEmptyPlaceholder message="No jobs found" />}
+    </>
+  );
 }

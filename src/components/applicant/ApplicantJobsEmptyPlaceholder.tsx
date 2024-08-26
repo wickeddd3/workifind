2e0ec45928c11b3a9b3b288e256da036e24aebef
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-27 03:16:02 +0800

    feat: add ApplicantJobsEmptyPlaceholder component

---
+++ src/components/applicant/ApplicantJobsEmptyPlaceholder.tsx
@@ -0,0 +1,16 @@
+import IllustrationJobs from "@/components/illustrations/IllustrationJobs";
+
+interface ApplicantJobsEmptyPlaceholderProps {
+  message?: string;
+}
+
+export default function ApplicantJobsEmptyPlaceholder({
+  message,
+}: ApplicantJobsEmptyPlaceholderProps) {
+  return (
+    <div className="flex h-[60vh] flex-col items-center justify-center gap-6 rounded-2xl bg-gray-50 px-4 py-8">
+      <IllustrationJobs />
+      <h4 className="text-lg font-semibold">{message}</h4>
+    </div>
+  );
+}

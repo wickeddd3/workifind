5703b49925ece665a6108d26adb74757f9ef65a6
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-27 04:12:39 +0800

    cut: remove sign in link

--- src/components/home/saved/SavedJobsUnauthenticated.tsx
+++ src/components/home/saved/SavedJobsUnauthenticated.tsx
@@ -1,15 +1,11 @@
 import IllustrationAuthentication from "@/components/illustrations/IllustrationAuthentication";
-import { clerkSignInUrl } from "@/lib/clerk";
 
 export default function SavedJobsUnauthenticated() {
   return (
     <div className="flex flex-col items-center justify-center gap-2 rounded-2xl bg-gray-50 px-4 py-8">
       <IllustrationAuthentication />
       <p className="text-md text-center font-normal text-muted-foreground">
-        <a href={clerkSignInUrl} className="font-semibold underline">
-          Sign in
-        </a>{" "}
-        and start saving jobs that match your preferences
+        Sign in and start saving jobs that match your preferences
       </p>
     </div>
   );

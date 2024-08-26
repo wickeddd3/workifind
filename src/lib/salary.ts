4fa3fc39879c2eb3c1acb2857160ff538ac5ac5c
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-26 21:47:06 +0800

    feat: add salary util

---
+++ src/lib/salary.ts
@@ -0,0 +1,21 @@
+import { formatMoney } from "@/lib/utils";
+import { Job } from "@prisma/client";
+
+export const hasJobSalary = (job: Job) => {
+  const { minSalary, maxSalary } = job;
+  if (!minSalary && !maxSalary) {
+    return false;
+  }
+  return true;
+};
+
+export const getJobSalary = (job: Job) => {
+  const { minSalary, maxSalary } = job;
+  if (minSalary === maxSalary) {
+    return formatMoney(minSalary);
+  }
+  if (!minSalary || !maxSalary) {
+    return formatMoney(maxSalary);
+  }
+  return `${formatMoney(minSalary)} - ${formatMoney(maxSalary)}`;
+};

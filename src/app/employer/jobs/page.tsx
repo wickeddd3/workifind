e4dc38cda8b3fd3de52ab7b3fa21a6584a7a5895
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-28 00:42:44 +0800

    feat: add employer jobs pagination

--- src/app/employer/jobs/page.tsx
+++ src/app/employer/jobs/page.tsx
@@ -2,34 +2,61 @@
 
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { useUser } from "@/contexts/UserContext";
-import { getEmployerJobs } from "@/actions/jobs";
+import { getEmployerJobs, getEmployerJobsCount } from "@/actions/jobs";
 import EmployerJobs from "@/components/employer/EmployerJobs";
 import { Job, JobApplication } from "@prisma/client";
 import EmployerJobsEmptyPlaceholder from "@/components/employer/EmployerJobsEmptyPlaceholder";
 import EmployerJobsLoadingPlaceholder from "@/components/employer/EmployerJobsLoadingPlaceholder";
 
-export default function Page() {
+interface PageProps {
+  searchParams: {
+    page?: string;
+  };
+}
+
+export default function Page({ searchParams: { page } }: PageProps) {
   const { user } = useUser();
   const [jobs, setJobs] = useState<
     (Job & { jobApplications: JobApplication[] })[] | null
   >(null);
-
+  const [totalResults, setTotalResults] = useState(0);
   const hasEmptyJobs = useMemo(() => jobs && jobs.length === 0, [jobs]);
+  const currentPage = page ? parseInt(page) : 1;
+  const jobsPerPage = 5;
+  const skip = (currentPage - 1) * jobsPerPage;
 
   const handleGetJobs = useCallback(
-    async (id: number) => await getEmployerJobs(id),
-    [],
+    async (id: number) => {
+      const [jobs, totalResults] = await Promise.all([
+        getEmployerJobs({
+          id,
+          take: jobsPerPage,
+          skip,
+        }),
+        getEmployerJobsCount(id),
+      ]);
+      setJobs(jobs);
+      setTotalResults(totalResults);
+    },
+    [skip],
   );
 
   useEffect(() => {
     if (user && user?.id) {
-      handleGetJobs(user.id).then(setJobs);
+      handleGetJobs(user.id);
     }
   }, [user, handleGetJobs]);
 
   return (
     <>
-      {jobs && <EmployerJobs jobs={jobs} />}
+      {jobs && (
+        <EmployerJobs
+          jobs={jobs}
+          totalResults={totalResults}
+          jobsPerPage={jobsPerPage}
+          page={currentPage}
+        />
+      )}
       {!jobs && <EmployerJobsLoadingPlaceholder />}
       {hasEmptyJobs && <EmployerJobsEmptyPlaceholder message="No jobs found" />}
     </>

4d2c648335728f82d9886b23724ad4e13c5f921c
wickeddd3 <pablophilipandrew@gmail.com>
2024-08-27 23:28:06 +0800

    feat: add applicant job applications pagination

--- src/app/applicant/jobs/page.tsx
+++ src/app/applicant/jobs/page.tsx
@@ -3,44 +3,54 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { useUser } from "@/contexts/UserContext";
 import ApplicantJobApplications from "@/components/applicant/ApplicantJobApplications";
-import { getApplicantJobApplications } from "@/actions/jobApplications";
+import {
+  getApplicantJobApplications,
+  getApplicantJobApplicationsCount,
+} from "@/actions/jobApplications";
 import { getApplicant } from "@/actions/applicants";
 import ApplicantJobsEmptyPlaceholder from "@/components/applicant/ApplicantJobsEmptyPlaceholder";
 import { Job, JobApplication } from "@prisma/client";
 import ApplicantJobsLoadingPlaceholder from "@/components/applicant/ApplicantJobsLoadingPlaceholder";
 
-export default function Page() {
+interface PageProps {
+  searchParams: {
+    page?: string;
+  };
+}
+
+export default function Page({ searchParams: { page } }: PageProps) {
   const { user } = useUser();
   const [applicant, setApplicant] = useState(null);
   const [jobApplications, setJobApplications] = useState<
     (JobApplication & { job: Job })[] | null
   >(null);
-
+  const [totalResults, setTotalResults] = useState(0);
   const hasEmptyJobApplications = useMemo(
     () => jobApplications && jobApplications.length === 0,
     [jobApplications],
   );
-
-  const handleGetApplicant = useCallback(
-    async (id: number) => await getApplicant(id),
-    [],
-  );
-
-  const handleGetJobApplications = useCallback(
-    async (id: number) => await getApplicantJobApplications(id),
-    [],
-  );
+  const currentPage = page ? parseInt(page) : 1;
+  const jobsPerPage = 5;
+  const skip = (currentPage - 1) * jobsPerPage;
 
   const handleGetApplicantJobApplications = useCallback(
     async (userId: number) => {
-      const applicant = await handleGetApplicant(userId);
+      const applicant = await getApplicant(userId);
       setApplicant(applicant);
       if (applicant) {
-        const jobApplications = await handleGetJobApplications(applicant.id);
+        const [jobApplications, totalResults] = await Promise.all([
+          getApplicantJobApplications({
+            id: applicant.id,
+            take: jobsPerPage,
+            skip,
+          }),
+          getApplicantJobApplicationsCount(applicant.id),
+        ]);
         setJobApplications(jobApplications);
+        setTotalResults(totalResults);
       }
     },
-    [handleGetApplicant, handleGetJobApplications],
+    [skip],
   );
 
   useEffect(() => {
@@ -52,7 +62,12 @@ export default function Page() {
   return (
     <>
       {applicant && jobApplications && (
-        <ApplicantJobApplications jobApplications={jobApplications} />
+        <ApplicantJobApplications
+          jobApplications={jobApplications}
+          totalResults={totalResults}
+          jobsPerPage={jobsPerPage}
+          page={currentPage}
+        />
       )}
       {!applicant && !hasEmptyJobApplications && (
         <ApplicantJobsLoadingPlaceholder />
--- src/components/applicant/ApplicantJobApplications.tsx
+++ src/components/applicant/ApplicantJobApplications.tsx
@@ -1,24 +1,82 @@
 import { Job, JobApplication } from "@prisma/client";
 import ApplicantJobApplicationItem from "@/components/applicant/ApplicantJobApplicationItem";
+import Link from "next/link";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 interface ApplicantJobApplicationsProps {
   jobApplications: (JobApplication & { job: Job })[];
+  totalResults: number;
+  jobsPerPage: number;
+  page: number;
 }
 
 export default function ApplicantJobApplications({
   jobApplications,
+  totalResults,
+  jobsPerPage,
+  page,
 }: ApplicantJobApplicationsProps) {
   return (
     <main className="m-auto space-y-6 px-4">
       <h1 className="px-4 text-xl font-medium">Applied jobs</h1>
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-4">
         {jobApplications.map((jobApplication) => (
           <ApplicantJobApplicationItem
             jobApplication={jobApplication}
             key={jobApplication.id}
           />
         ))}
+        {jobApplications.length > 0 && (
+          <Pagination
+            currentPage={page}
+            totalPages={Math.ceil(totalResults / jobsPerPage)}
+          />
+        )}
       </div>
     </main>
+  );
+}
+
+interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+}
+
+function Pagination({ currentPage, totalPages }: PaginationProps) {
+  function generatePageLink(page: number) {
+    const searchParams = new URLSearchParams({
+      ...(page && { page: page.toString() }),
+    });
+
+    return `/applicant/jobs?${searchParams.toString()}`;
+  }
+
+  return (
+    <div className="flex justify-between">
+      <Link
+        href={generatePageLink(currentPage - 1)}
+        className={cn(
+          "flex items-center gap-2 font-semibold",
+          currentPage <= 1 && "invisible",
+        )}
+      >
+        <ArrowLeft size={16} />
+        Previous page
+      </Link>
+      <span className="font-semibold">
+        Page {currentPage} of {totalPages}
+      </span>
+      <Link
+        href={generatePageLink(currentPage + 1)}
+        className={cn(
+          "flex items-center gap-2 font-semibold",
+          currentPage >= totalPages && "invisible",
+        )}
+      >
+        Next page
+        <ArrowRight size={16} />
+      </Link>
+    </div>
   );
 }

d8c31da659fc104a7b05f4d6a9b218d36b5b9825
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-07 15:27:39 +0800

    add: responsive styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,34 +1,79 @@
 import Link from "next/link";
+import { FaSquareFacebook } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <footer className="border-t">
-      <div className="mx-auto max-w-5xl space-y-5 px-3 py-5">
-        <div className="flex flex-col justify-between gap-3 sm:flex-row sm:items-center">
-          <div className="space-y-2">
-            <h3 className="font-mono text-xl">workifind</h3>
-            <p className="text-sm text-muted-foreground">
+    <footer className="bg-gray-white">
+      <div className="mx-auto max-w-7xl space-y-5 px-3 py-5">
+        <div className="flex flex-col flex-wrap items-center justify-center gap-3 md:flex-row md:justify-between">
+          <div className="flex flex-col items-center gap-2 md:items-start">
+            <div className="flex w-fit items-center gap-2">
+              <div className="flex h-8 w-8 items-center justify-center rounded-full bg-indigo-700 text-neutral-100">
+                <h3 className="text-center align-middle text-lg font-extrabold">
+                  W
+                </h3>
+              </div>
+              <h3 className="text-xl font-extrabold tracking-wider text-gray-800">
+                workifind
+              </h3>
+            </div>
+            <p className="text-sm font-medium text-gray-900">
               Connecting talents with opportunities
             </p>
           </div>
-          <div className="flex flex-wrap gap-5 text-sm text-muted-foreground">
-            <Link href="/about" className="hover:underline">
-              About Us
-            </Link>
-            <Link href="/contact" className="hover:underline">
-              Contact
-            </Link>
-            <Link href="/terms" className="hover:underline">
-              Terms of Service
-            </Link>
-            <Link href="/privacy" className="hover:underline">
-              Privacy Policy
-            </Link>
+          <div className="flex flex-col items-center gap-4">
+            <div className="flex flex-wrap items-center justify-center gap-5">
+              <Link
+                href="/about"
+                className="text-xs font-medium text-gray-900 hover:text-indigo-700 md:text-sm"
+              >
+                About Us
+              </Link>
+              <Link
+                href="/contact"
+                className="text-xs font-medium text-gray-900 hover:text-indigo-700 md:text-sm"
+              >
+                Contact
+              </Link>
+              <Link
+                href="/terms"
+                className="text-xs font-medium text-gray-900 hover:text-indigo-700 md:text-sm"
+              >
+                Terms of Service
+              </Link>
+              <Link
+                href="/privacy"
+                className="text-xs font-medium text-gray-900 hover:text-indigo-700 md:text-sm"
+              >
+                Privacy Policy
+              </Link>
+            </div>
+            <div className="flex flex-wrap items-center justify-center gap-2">
+              <Link href="" className="rounded-lg p-1 hover:bg-indigo-100">
+                <FaSquareFacebook size="1.5em" color="#3a4955" />
+              </Link>
+              <Link href="" className="rounded-lg p-1 hover:bg-indigo-100">
+                <FaLinkedin size="1.5em" color="#3a4955" />
+              </Link>
+
+              <Link href="" className="rounded-lg p-1 hover:bg-indigo-100">
+                <FaInstagram size="1.5em" color="#3a4955" />
+              </Link>
+
+              <Link href="" className="rounded-lg p-1 hover:bg-indigo-100">
+                <FaXTwitter size="1.5em" color="#3a4955" />
+              </Link>
+            </div>
           </div>
         </div>
-        <div className="text-center text-sm text-muted-foreground">
-          c {new Date().getFullYear()} workifind, Inc. All rights reserved.
-        </div>
+        <h6 className="text-center text-xs font-semibold md:text-sm">
+          &copy; {`${currentYear} workifind`}
+        </h6>
       </div>
     </footer>
   );

956ea20f8b8807d0e53b822d6564e0c4ce426130
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-07 15:27:29 +0800

    add: screen and fontSize tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,29 @@ const config = {
   ],
   prefix: "",
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    fontSize: {
+      "4xs": ".375rem", // 6px
+      "3xs": ".5rem", // 8px
+      "2xs": ".625rem", // 10px
+      xs: ".75rem", // 12px
+      sm: ".875rem", // 14px
+      md: "1rem", // 16px
+      lg: "1.125rem", // 18px
+      xl: "1.25rem", // 20px
+      "2xl": "1.5rem", // 24px
+      "3xl": "1.875rem", // 30px
+      "4xl": "2.25rem", // 36px
+      "5xl": "3rem", // 48px
+      "6xl": "4rem", // 64px
+      "7xl": "5rem", // 80px
+      "8xl": "6rem", // 96px
+    },
     container: {
       center: true,
       padding: "2rem",

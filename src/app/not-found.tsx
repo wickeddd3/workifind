89402fe3bf92200208b782e90164cd2332369085
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-10 02:42:16 +0800

    add: not-found svg

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <main className="m-auto flex h-[70vh] w-full max-w-7xl items-center justify-center md:h-[80vh]">
+      <section className="bg-not-found flex h-1/2 w-full flex-col items-center justify-end bg-center bg-no-repeat md:w-1/2">
+        <Link
+          href="/"
+          className="z-10 rounded-xl bg-emerald-500 px-4 py-2 font-bold text-white shadow-md"
+        >
+          Go to homepage
+        </Link>
+      </section>
+    </main>
+  );
+}

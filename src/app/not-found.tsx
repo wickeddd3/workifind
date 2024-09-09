68ae89e619a387ef799371b193c03eb0c625aa92
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-10 02:51:59 +0800

    lint: fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <main className="m-auto flex h-[70vh] w-full max-w-7xl items-center justify-center md:h-[80vh]">
-      <section className="bg-not-found flex h-1/2 w-full flex-col items-center justify-end bg-center bg-no-repeat md:w-1/2">
+      <section className="flex h-1/2 w-full flex-col items-center justify-end bg-not-found bg-center bg-no-repeat md:w-1/2">
         <Link
           href="/"
           className="z-10 rounded-xl bg-emerald-500 px-4 py-2 font-bold text-white shadow-md"

10fd32a16a787429faa2f05668a9cd7242e305f6
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-30 04:25:44 +0800

    feat: use findCompanyBySlug and cache function

--- src/app/companies/[slug]/page.tsx
+++ src/app/companies/[slug]/page.tsx
@@ -1,12 +1,22 @@
-import { getCompany } from "@/actions/companies";
 import CompanyDetails from "@/components/companies/CompanyDetails";
+import { cache } from "react";
+import { notFound } from "next/navigation";
+import { findCompanyBySlug } from "@/app/_services/company";
 
 interface PageProps {
   params: { slug: string };
 }
 
+const handleFindCompanyBySlug = cache(async (slug: string) => {
+  const company = await findCompanyBySlug(slug);
+
+  if (!company) notFound();
+
+  return company;
+});
+
 export default async function Page({ params: { slug } }: PageProps) {
-  const company = await getCompany(slug);
+  const company = await handleFindCompanyBySlug(slug);
 
   return (
     <main className="mx-auto h-full max-w-4xl p-4">

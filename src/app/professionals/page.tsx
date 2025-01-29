dc939fcb132016afece88ab8a6ba3302d2a9a454
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-30 01:26:59 +0800

    feat: use getInitialProfessionalList service and cache function

--- src/app/professionals/page.tsx
+++ src/app/professionals/page.tsx
@@ -1,10 +1,15 @@
-import { getInitialProfessionals } from "@/actions/professionals";
 import ProfessionalFilter from "@/components/professionals/ProfessionalFilter";
 import ProfessionalInitialList from "@/components/professionals/ProfessionalInitialList";
 import ProfessionalSearchTip from "@/components/professionals/ProfessionalSearchTip";
+import { cache } from "react";
+import { getInitialProfessionalList } from "@/app/_services/professionals";
+
+const handleGetInitialProfessionalList = cache(async () => {
+  return await getInitialProfessionalList();
+});
 
 export default async function Page() {
-  const professionals = await getInitialProfessionals();
+  const professionals = await handleGetInitialProfessionalList();
 
   return (
     <main className="m-auto flex max-w-7xl flex-col gap-6 px-3 py-12">

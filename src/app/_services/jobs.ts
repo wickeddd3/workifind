d3e539227b8bf242fbf6e287067b66fec4d7f106
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-29 20:42:34 +0800

    feat: add filterJobs and filterJobsCount service

---
+++ src/app/_services/jobs.ts
@@ -0,0 +1,62 @@
+import { baseUrl } from "@/lib/baseUrl";
+
+export interface FilterQuery {
+  searchQuery: string;
+  employmentType: string;
+  salary: string;
+  locationType: string;
+  jobsPerPage?: number;
+  page?: number;
+}
+
+export const filterJobs = async ({
+  searchQuery,
+  employmentType,
+  salary,
+  locationType,
+  jobsPerPage,
+  page,
+}: FilterQuery) => {
+  try {
+    const searchParams = new URLSearchParams({
+      ...(searchQuery && { searchQuery: searchQuery.trim() }),
+      ...(employmentType && { employmentType }),
+      ...(salary && { salary }),
+      ...(locationType && { locationType }),
+      ...(jobsPerPage && { jobsPerPage: jobsPerPage.toString() }),
+      ...(page && { page: page.toString() }),
+    });
+
+    const response = await fetch(
+      `${baseUrl}/api/jobs?${searchParams.toString()}`,
+    );
+
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const filterJobsCount = async ({
+  searchQuery,
+  employmentType,
+  salary,
+  locationType,
+}: FilterQuery) => {
+  try {
+    const searchParams = new URLSearchParams({
+      ...(searchQuery && { searchQuery: searchQuery.trim() }),
+      ...(employmentType && { employmentType }),
+      ...(salary && { salary }),
+      ...(locationType && { locationType }),
+    });
+
+    const response = await fetch(
+      `${baseUrl}/api/jobs/count?${searchParams.toString()}`,
+    );
+
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};

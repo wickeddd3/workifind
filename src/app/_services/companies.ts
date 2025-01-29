f0b06097dfe57c60fdf09ec0c7027fd32c58d488
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-30 04:22:01 +0800

    feat: add searchCompanies and searchCompaniesCount service

--- src/app/_services/companies.ts
+++ src/app/_services/companies.ts
@@ -12,3 +12,43 @@ export const getInitialCompanyList = async (initialNumber?: number) => {
 
   return await response.json();
 };
+
+export const searchCompanies = async ({
+  searchQuery,
+  size,
+  page,
+}: {
+  searchQuery: string;
+  size: number;
+  page: number;
+}) => {
+  const params = {
+    searchQuery: searchQuery?.toString() || "",
+    size: size?.toString() ?? "10",
+    page: page?.toString() ?? "1",
+  };
+  const searchParams = new URLSearchParams(params).toString();
+
+  const response = await fetch(
+    `${baseUrl}/api/companies/search?${searchParams}`,
+  );
+
+  return response.json();
+};
+
+export const searchCompaniesCount = async ({
+  searchQuery,
+}: {
+  searchQuery: string;
+}) => {
+  const params = {
+    searchQuery: searchQuery?.toString() || "",
+  };
+  const searchParams = new URLSearchParams(params).toString();
+
+  const response = await fetch(
+    `${baseUrl}/api/companies/search/count?${searchParams}`,
+  );
+
+  return response.json();
+};

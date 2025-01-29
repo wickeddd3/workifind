917ba56116603a146e84911431c3d877102aa982
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-30 03:34:32 +0800

    feat: use findProfessionalById service and cache function

--- src/app/professionals/[id]/page.tsx
+++ src/app/professionals/[id]/page.tsx
@@ -1,13 +1,23 @@
-import { getProfessional } from "@/actions/professionals";
 import ProfessionalDetails from "@/components/professionals/ProfessionalDetails";
+import { cache } from "react";
+import { notFound } from "next/navigation";
+import { findProfessionalById } from "@/app/_services/professional";
 
 interface PageProps {
   params: { id: string };
 }
 
+const handleFindProfessionalById = cache(async (id: number) => {
+  const professional = await findProfessionalById(id);
+
+  if (!professional) notFound();
+
+  return professional;
+});
+
 export default async function Page({ params: { id } }: PageProps) {
   const professionalId = parseInt(id);
-  const professional = await getProfessional(professionalId);
+  const professional = await handleFindProfessionalById(professionalId);
 
   return (
     <main className="mx-auto h-full max-w-4xl p-4">

07e76fc52e5f8011da37152b278fcf78c53d5e1d
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-30 04:20:20 +0800

    refactor: /api/companies/search api route

--- src/app/api/companies/search/route.ts
+++ src/app/api/companies/search/route.ts
@@ -3,34 +3,40 @@ import { NextResponse } from "next/server";
 import { type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams;
-  const queryParam = searchParams.get("q") ?? "";
-  const takeParam = searchParams.get("take");
-  const skipParam = searchParams.get("skip");
-  const take = takeParam ? parseInt(takeParam) : 10;
-  const skip = skipParam ? parseInt(skipParam) : 0;
-
   try {
-    const employers = await prisma.employer.findMany({
+    // Parse the URL
+    const { searchParams } = new URL(request.url);
+    // Destructure query parameters
+    const {
+      searchQuery = "",
+      size = "10",
+      page = "1",
+    } = Object.fromEntries(searchParams);
+    // Calculate the number of rows to skip
+    const rowsToSkip = (parseInt(page) - 1) * parseInt(size);
+    // Parse size to integer
+    const take = size ? parseInt(size) : 10;
+    // Fetch companies from the database
+    const companies = await prisma.employer.findMany({
       orderBy: { createdAt: "desc" },
       where: {
         companyName: {
-          contains: queryParam,
+          contains: searchQuery,
           mode: "insensitive",
         },
       },
-      take,
-      skip,
+      take, // limit,
+      skip: rowsToSkip, // offset,
     });
 
-    if (!employers) {
+    if (!companies) {
       return NextResponse.json(
         { error: "No companies match on your query" },
         { status: 404 },
       );
     }
 
-    return NextResponse.json({ companies: employers }, { status: 200 });
+    return NextResponse.json(companies, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       { error: "Error searching companies" },

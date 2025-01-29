d5e670d8606784d4726d3565efc78bc803057dff
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-30 03:31:11 +0800

    feat: add searchProfessionals and searchProfessionalsCount service

--- src/app/_services/professionals.ts
+++ src/app/_services/professionals.ts
@@ -12,3 +12,43 @@ export const getInitialProfessionalList = async (initialNumber?: number) => {
 
   return await response.json();
 };
+
+export const searchProfessionals = async ({
+  searchQuery,
+  size,
+  page,
+}: {
+  searchQuery: string;
+  size: number;
+  page: number;
+}) => {
+  const params = {
+    searchQuery: searchQuery?.toString() || "",
+    size: size?.toString() ?? "10",
+    page: page?.toString() ?? "1",
+  };
+  const searchParams = new URLSearchParams(params).toString();
+
+  const response = await fetch(
+    `${baseUrl}/api/professionals/search?${searchParams}`,
+  );
+
+  return response.json();
+};
+
+export const searchProfessionalsCount = async ({
+  searchQuery,
+}: {
+  searchQuery: string;
+}) => {
+  const params = {
+    searchQuery: searchQuery?.toString() || "",
+  };
+  const searchParams = new URLSearchParams(params).toString();
+
+  const response = await fetch(
+    `${baseUrl}/api/professionals/search/count?${searchParams}`,
+  );
+
+  return response.json();
+};

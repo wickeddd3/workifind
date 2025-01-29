054019d4dcd2a9b1350f35ca83e58c5a81875831
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-29 20:46:41 +0800

    feat: add filterJobs and filterJobsCount fetch logic, JobResultsEmptyPlaceholder, and JobResultsPagination component

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,7 +1,10 @@
 import JobFilter from "@/components/jobs/JobFilter";
 import JobResults from "@/components/jobs/JobResults";
+import JobResultsEmptyPlaceholder from "@/components/jobs/JobResultsEmptyPlaceholder";
+import JobResultsPagination from "@/components/jobs/JobResultsPagination";
 import JobSelected from "@/components/jobs/JobSelected";
 import { JobFilterValues } from "@/lib/validation";
+import { filterJobs, filterJobsCount } from "@/app/_services/jobs";
 
 interface PageProps {
   searchParams: {
@@ -14,7 +17,7 @@ interface PageProps {
   };
 }
 
-export default function Page({
+export default async function Page({
   searchParams: { q, employmentType, salary, locationType, job, page },
 }: PageProps) {
   const filterValues: JobFilterValues = {
@@ -23,16 +26,50 @@ export default function Page({
     salary,
     locationType,
   };
+  const jobsPerPage = 10;
+  const currentPage = page ? parseInt(page) : 1;
+  const jobSlug = job;
+  const query = {
+    searchQuery: q ?? "",
+    employmentType: employmentType ?? "",
+    salary: salary ?? "",
+    locationType: locationType ?? "",
+  };
+  const filterJobsQuery = {
+    ...query,
+    jobsPerPage,
+    page: currentPage,
+  };
+
+  const [jobs, jobsCount] = await Promise.all([
+    filterJobs(filterJobsQuery),
+    filterJobsCount(query),
+  ]);
+
+  const hasJobs = jobs && jobs.length > 0;
 
   return (
     <main className="m-auto mb-10 space-y-6">
       <JobFilter defaultValues={filterValues} />
       <section className="m-auto flex h-full max-w-7xl gap-4 px-3 md:flex-row">
-        <JobResults
-          filterValues={filterValues}
-          page={page ? parseInt(page) : undefined}
-          jobSlug={job}
-        />
+        <div className="w-full space-y-4 py-2 md:w-2/5">
+          <JobResults
+            jobs={jobs}
+            filterValues={filterValues}
+            page={currentPage}
+          />
+          {hasJobs && (
+            <>
+              <JobResultsEmptyPlaceholder />
+              <JobResultsPagination
+                currentPage={currentPage}
+                totalPages={Math.ceil(jobsCount / jobsPerPage)}
+                filterValues={filterValues}
+                jobSlug={jobSlug}
+              />
+            </>
+          )}
+        </div>
         <JobSelected jobSlug={job} />
       </section>
     </main>

a7f5988e7aa95e8e991313e4a7ac8f52673cf5d1
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-29 20:44:06 +0800

    feat: add JobResultsEmptyPlaceholder component

---
+++ src/components/jobs/JobResultsEmptyPlaceholder.tsx
@@ -0,0 +1,7 @@
+export default function JobResultsEmptyPlaceholder() {
+  return (
+    <p className="m-auto text-center">
+      No jobs found. Try adjusting your search filters.
+    </p>
+  );
+}

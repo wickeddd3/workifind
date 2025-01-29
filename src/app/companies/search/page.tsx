2a4bd37854ecfac6b81ab66253a1e624d5793c0b
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-30 04:25:08 +0800

    feat: use searchCompanies and searchCompaniesCount service. and use CompanySearchResultsPagination

--- src/app/companies/search/page.tsx
+++ src/app/companies/search/page.tsx
@@ -1,6 +1,12 @@
 import CompanySearchFilter from "@/components/companies/CompanySearchFilter";
 import CompanySearchResults from "@/components/companies/CompanySearchResults";
+import CompanySearchEmptyPlaceholder from "@/components/companies/CompanySearchEmptyPlaceholder";
+import CompanySearchResultsPagination from "@/components/companies/CompanySearchResultsPagination";
 import { CompanyFilterValues } from "@/lib/validation";
+import {
+  searchCompanies,
+  searchCompaniesCount,
+} from "@/app/_services/companies";
 
 interface PageProps {
   searchParams: {
@@ -9,18 +15,44 @@ interface PageProps {
   };
 }
 
-export default function Page({ searchParams: { q, page } }: PageProps) {
+export default async function Page({ searchParams: { q, page } }: PageProps) {
   const filterValues: CompanyFilterValues = {
     q,
   };
+  const jobsPerPage = 10;
+  const currentPage = page ? parseInt(page) : 1;
+  const query = {
+    searchQuery: q ?? "",
+  };
+  const searchCompaniesQuery = {
+    ...query,
+    size: jobsPerPage,
+    page: currentPage,
+  };
+
+  const [companies, totalResults] = await Promise.all([
+    searchCompanies(searchCompaniesQuery),
+    searchCompaniesCount(query),
+  ]);
+
+  const hasCompanies = companies && companies.length > 0;
 
   return (
     <main className="m-auto mb-10 flex h-full min-h-screen max-w-2xl flex-col gap-6 px-3">
       <CompanySearchFilter filterValues={filterValues} />
-      <CompanySearchResults
-        filterValues={filterValues}
-        page={page ? parseInt(page) : undefined}
-      />
+      <div className="flex h-full w-full flex-col gap-6">
+        {hasCompanies && (
+          <>
+            <CompanySearchResults companies={companies} />
+            <CompanySearchResultsPagination
+              currentPage={currentPage}
+              totalPages={Math.ceil(totalResults / jobsPerPage)}
+              filterValues={filterValues}
+            />
+          </>
+        )}
+        {!hasCompanies && <CompanySearchEmptyPlaceholder />}
+      </div>
     </main>
   );
 }

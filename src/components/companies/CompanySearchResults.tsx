066f66004401480878fdc5db718a4c2359e997f7
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-30 04:24:06 +0800

    refactor: move pagination and empty placeholder to search companies page

--- src/components/companies/CompanySearchResults.tsx
+++ src/components/companies/CompanySearchResults.tsx
@@ -1,101 +1,21 @@
-import { searchCompanies, searchCompaniesCount } from "@/actions/companies";
 import CompanySearchResultItem from "@/components/companies/CompanySearchResultItem";
-import { cn } from "@/lib/utils";
-import { CompanyFilterValues } from "@/lib/validation";
 import { Employer } from "@prisma/client";
-import { ArrowLeft, ArrowRight } from "lucide-react";
 import Link from "next/link";
-import CompanySearchEmptyPlaceholder from "@/components/companies/CompanySearchEmptyPlaceholder";
 
 interface CompanySearchResultsProps {
-  filterValues: CompanyFilterValues;
-  page?: number;
+  companies: Employer[];
 }
 
 export default async function CompanySearchResults({
-  filterValues,
-  page = 1,
+  companies,
 }: CompanySearchResultsProps) {
-  const { q } = filterValues;
-  const jobsPerPage = 10;
-  const skip = (page - 1) * jobsPerPage;
-  const query = {
-    query: q ?? "",
-    take: jobsPerPage,
-    skip,
-  };
-
-  const [companies, totalResults] = await Promise.all([
-    searchCompanies(query),
-    searchCompaniesCount(q ?? ""),
-  ]);
-
   return (
-    <div className="flex h-full w-full flex-col gap-6">
-      <div className="flex h-full w-full flex-col gap-4">
-        {companies.map((company: Employer) => (
-          <Link href={`/companies/${company.slug}`} key={company.slug}>
-            <CompanySearchResultItem company={company} />
-          </Link>
-        ))}
-      </div>
-      {companies.length === 0 && <CompanySearchEmptyPlaceholder />}
-      {companies.length > 0 && (
-        <Pagination
-          currentPage={page}
-          totalPages={Math.ceil(totalResults / jobsPerPage)}
-          filterValues={filterValues}
-        />
-      )}
-    </div>
-  );
-}
-
-interface PaginationProps {
-  currentPage: number;
-  totalPages: number;
-  filterValues: CompanyFilterValues;
-}
-
-function Pagination({
-  currentPage,
-  totalPages,
-  filterValues: { q },
-}: PaginationProps) {
-  function generatePageLink(page: number) {
-    const searchParams = new URLSearchParams({
-      ...(q && { q: q.trim() }),
-      ...(page && { page: page.toString() }),
-    });
-
-    return `/companies/search?${searchParams.toString()}`;
-  }
-
-  return (
-    <div className="flex justify-between">
-      <Link
-        href={generatePageLink(currentPage - 1)}
-        className={cn(
-          "flex items-center gap-2 text-sm font-semibold text-gray-950 md:text-md",
-          currentPage <= 1 && "invisible",
-        )}
-      >
-        <ArrowLeft size={16} />
-        Previous page
-      </Link>
-      <span className="text-sm font-semibold text-gray-950 md:text-md">
-        Page {currentPage} of {totalPages}
-      </span>
-      <Link
-        href={generatePageLink(currentPage + 1)}
-        className={cn(
-          "flex items-center gap-2 text-sm font-semibold text-gray-950 md:text-md",
-          currentPage >= totalPages && "invisible",
-        )}
-      >
-        Next page
-        <ArrowRight size={16} />
-      </Link>
+    <div className="flex h-full w-full flex-col gap-4">
+      {companies.map((company) => (
+        <Link href={`/companies/${company.slug}`} key={company.slug}>
+          <CompanySearchResultItem company={company} />
+        </Link>
+      ))}
     </div>
   );
 }

2e7f1de491ac89ecccf0932a52c2f1c2199a45fd
wickeddd3 <pablophilipandrew@gmail.com>
2025-02-03 04:45:46 +0800

    feat: use findJobBySlug service

--- src/app/jobs/[slug]/page.tsx
+++ src/app/jobs/[slug]/page.tsx
@@ -1,22 +1,20 @@
+import JobDetails from "@/components/jobs/JobDetails";
 import { cache } from "react";
 import { notFound } from "next/navigation";
-import JobDetails from "@/components/jobs/JobDetails";
-import { getJob } from "@/actions/jobs";
+import { findJobBySlug } from "@/app/_services/job";
 
 interface PageProps {
   params: { slug: string };
 }
 
-const handleGetJob = cache(async (slug: string) => {
-  const job = await getJob(slug);
-
-  if (!job) notFound();
-
-  return job;
+const handleFetchJob = cache(async (slug: string) => {
+  return await findJobBySlug(slug);
 });
 
 export default async function Page({ params: { slug } }: PageProps) {
-  const job = await handleGetJob(slug);
+  const job = await handleFetchJob(slug);
+
+  if (!job) notFound();
 
   return (
     <main className="mx-auto max-w-4xl p-4">

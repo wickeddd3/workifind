d6f5a2e3d2cc2742d3024c679b05f446aff86061
wickeddd3 <pablophilipandrew@gmail.com>
2025-02-02 17:59:46 +0800

    feat: use auth userId of clerk and add error handling if userId doesn't exist

--- src/app/api/employers/create/route.ts
+++ src/app/api/employers/create/route.ts
@@ -1,22 +1,35 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
 
 export async function POST(request: Request) {
-  const requestBody = await request.json();
-  const { userId, form } = requestBody;
-
   try {
+    const requestBody = await request.json();
+    const { form } = requestBody;
+    const { userId } = auth();
+
+    if (!userId) {
+      return NextResponse.json(
+        {
+          error:
+            "Error occurred, only authenticated user can create employer profile",
+        },
+        { status: 401 },
+      );
+    }
+
     const employer = await prisma.employer.create({
       data: {
-        userId,
         ...form,
+        userId,
       },
     });
 
-    return NextResponse.json({ employer }, { status: 200 });
+    return NextResponse.json(employer, { status: 200 });
   } catch (error) {
+    console.log(error);
     return NextResponse.json(
-      { error: "Error occurred while creating employer" },
+      { error: "Error occurred while creating employer profile" },
       { status: 500 },
     );
   }

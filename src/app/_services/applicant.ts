2d0d922cec8e697be0f6b322d54fe9bc45da949b
wickeddd3 <pablophilipandrew@gmail.com>
2025-02-02 18:34:26 +0800

    feat: add getApplicantProfileByUserId and updateApplicantProfile service

--- src/app/_services/applicant.ts
+++ src/app/_services/applicant.ts
@@ -41,3 +41,55 @@ export const createApplicantProfile = async (
     return { error: message };
   }
 };
+
+export const getApplicantProfileByUserId = async (userId: string) => {
+  const response = await fetch(`${baseUrl}/api/applicants/${userId}`);
+  const data = await response.json();
+
+  if (data?.error) return null;
+
+  return {
+    ...data,
+    skills: data?.skills?.map((item: string) => JSON.parse(item)),
+    languages: data?.languages?.map((item: string) => JSON.parse(item)),
+    preferredLocations: data?.preferredLocations?.map((item: string) =>
+      JSON.parse(item),
+    ),
+  };
+};
+
+export const updateApplicantProfile = async (
+  id: number,
+  formData: ApplicantProfileSchemaType,
+) => {
+  try {
+    //Prepare Form Data
+    const form = {
+      ...formData,
+      skills: formData.skills?.map((skill) => JSON.stringify(skill)),
+      languages: formData.languages?.map((language) =>
+        JSON.stringify(language),
+      ),
+      preferredLocations: formData.preferredLocations?.map(
+        (preferredLocation) => JSON.stringify(preferredLocation),
+      ),
+      salaryExpectation: parseInt(
+        formData?.salaryExpectation?.toString() || "0",
+      ),
+    };
+
+    // Update applicant profile
+    const response = await fetch(`${baseUrl}/api/applicants/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({ form }),
+    });
+
+    return await response.json();
+  } catch (error) {
+    let message = "Unexpected error";
+    if (error instanceof Error) {
+      message = error.message;
+    }
+    return { error: message };
+  }
+};

25347afc4276f929343de3fed85880207597d3c9
wickeddd3 <pablophilipandrew@gmail.com>
2025-02-03 23:41:22 +0800

    feat: use clerk useUser for identifying auth user id

--- src/components/jobs/SaveJobButton.tsx
+++ src/components/jobs/SaveJobButton.tsx
@@ -1,90 +1,77 @@
 "use client";
 
-import { getApplicant } from "@/actions/applicants";
-import { saveJob, unsaveJob } from "@/actions/jobs";
 import { Button } from "@/components/ui/button";
-import { useUser } from "@/contexts/UserContext";
-import { Applicant, Job, JobApplication, SavedJob } from "@prisma/client";
+import { Job, JobApplication } from "@prisma/client";
 import { useCallback, useEffect, useMemo, useState } from "react";
+import { useUser } from "@clerk/nextjs";
+import {
+  saveJob,
+  saveJobAuthorize,
+  unsaveJob,
+} from "@/app/_services/applicant-saved-jobs";
 
 interface SaveJobButtonProps {
   job: Job & { jobApplications: JobApplication[] };
 }
 
 export default function SaveJobButton({ job }: SaveJobButtonProps) {
-  const { user } = useUser();
-  const [applicant, setApplicant] = useState<
-    (Applicant & { savedJobs: SavedJob[] }) | null
-  >(null);
-
-  const handleGetApplicant = useCallback(
-    async (id: number) => await getApplicant(id),
-    [],
+  const { user, isSignedIn } = useUser();
+  const role = useMemo(() => user?.unsafeMetadata.role || "", [user]);
+  const isApplicant = useMemo(
+    () => isSignedIn && role === "APPLICANT",
+    [isSignedIn, role],
   );
+  const [isAuthorized, setIsAuthorized] = useState(false);
 
-  const handleCheckIfApplicantHasSavedThisJob = (
-    job: Job & { jobApplications: JobApplication[] },
-    applicant: (Applicant & { savedJobs: SavedJob[] }) | null,
-  ) => {
-    if (!applicant) return undefined;
-    const { savedJobs, id: applicantId } = applicant;
-    const { id: jobId } = job;
-    return (savedJobs || []).find(
-      (savedJob: SavedJob) =>
-        savedJob.jobId === jobId && savedJob.applicantId === applicantId,
-    );
-  };
-
-  const isApplicant = useMemo(() => {
-    return user && user?.role === "APPLICANT";
-  }, [user]);
-
-  const saveJobButtonText = useMemo(() => {
-    const savedJob: SavedJob | undefined =
-      handleCheckIfApplicantHasSavedThisJob(job, applicant);
-    if (savedJob) {
-      return "Unsave";
+  const handleCheckAuthorization = useCallback(async () => {
+    if (!user?.id) return;
+    try {
+      const authorized = await saveJobAuthorize(user.id, job.id);
+      setIsAuthorized(authorized);
+    } catch (error) {
+      console.error("Error checking authorization:", error);
+      setIsAuthorized(false);
     }
-    return "Save";
-  }, [job, applicant]);
+  }, [user, job]);
 
   const handleSaveJob = async () => {
-    await saveJob(applicant?.id, job?.id, user?.role, job?.slug);
-    if (user) handleGetApplicant(user?.id).then(setApplicant);
+    if (!user?.id) return;
+    await saveJob(user?.id, job.id);
+    handleCheckAuthorization();
   };
 
   const handleUnsaveJob = async () => {
-    await unsaveJob(applicant?.id, job?.id, user?.role, job?.slug);
-    if (user) handleGetApplicant(user?.id).then(setApplicant);
-  };
-
-  const handleSaveOrUnsaveJob = async () => {
-    const savedJob: SavedJob | undefined =
-      handleCheckIfApplicantHasSavedThisJob(job, applicant);
-    if (savedJob) {
-      return await handleUnsaveJob();
-    }
-
-    return await handleSaveJob();
+    if (!user?.id) return;
+    await unsaveJob(user?.id, job.id);
+    handleCheckAuthorization();
   };
 
   useEffect(() => {
-    if (user && user?.id) {
-      handleGetApplicant(user.id).then(setApplicant);
-    }
-  }, [user, handleGetApplicant]);
+    handleCheckAuthorization();
+  }, [handleCheckAuthorization]);
 
-  if (!user) return;
+  if (!isApplicant) return null;
 
-  if (isApplicant && applicant) {
-    return (
-      <Button
-        className="w-fit bg-indigo-600 px-8 hover:bg-indigo-700"
-        size="sm"
-        onClick={handleSaveOrUnsaveJob}
-      >
-        {saveJobButtonText}
-      </Button>
-    );
-  }
+  return (
+    <>
+      {isAuthorized && (
+        <Button
+          className="w-fit bg-indigo-600 px-8 hover:bg-indigo-700"
+          size="sm"
+          onClick={handleSaveJob}
+        >
+          Save
+        </Button>
+      )}
+      {!isAuthorized && (
+        <Button
+          className="w-fit bg-indigo-600 px-8 hover:bg-indigo-700"
+          size="sm"
+          onClick={handleUnsaveJob}
+        >
+          Unsave
+        </Button>
+      )}
+    </>
+  );
 }

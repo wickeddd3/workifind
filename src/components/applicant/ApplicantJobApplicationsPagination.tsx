983705e490e6aa79e0a04293c75d0c5a31674b7d
wickeddd3 <pablophilipandrew@gmail.com>
2025-02-03 23:37:48 +0800

    feat: add ApplicantJobApplicationsPagination component

---
+++ src/components/applicant/ApplicantJobApplicationsPagination.tsx
@@ -0,0 +1,49 @@
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+}
+
+export default function ApplicantJobApplicationsPagination({
+  currentPage,
+  totalPages,
+}: PaginationProps) {
+  function generatePageLink(page: number) {
+    const searchParams = new URLSearchParams({
+      ...(page && { page: page.toString() }),
+    });
+
+    return `/applicant/jobs?${searchParams.toString()}`;
+  }
+
+  return (
+    <div className="flex justify-between">
+      <Link
+        href={generatePageLink(currentPage - 1)}
+        className={cn(
+          "flex items-center gap-2 text-xs font-semibold text-gray-950 md:text-sm lg:text-md",
+          currentPage <= 1 && "invisible",
+        )}
+      >
+        <ArrowLeft size={16} />
+        Previous page
+      </Link>
+      <span className="text-xs font-semibold text-gray-950 md:text-sm lg:text-md">
+        Page {currentPage} of {totalPages}
+      </span>
+      <Link
+        href={generatePageLink(currentPage + 1)}
+        className={cn(
+          "flex items-center gap-2 text-xs font-semibold text-gray-950 md:text-sm lg:text-md",
+          currentPage >= totalPages && "invisible",
+        )}
+      >
+        Next page
+        <ArrowRight size={16} />
+      </Link>
+    </div>
+  );
+}

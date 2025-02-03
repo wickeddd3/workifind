fd5edbfd3c31344a22215b38518e136fe1a0d26b
wickeddd3 <pablophilipandrew@gmail.com>
2025-02-03 23:49:34 +0800

    cut: remove commented code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,4 @@
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-// import { authorizationMiddleware } from "@/middlewares/authorizationMiddleware";
-
-// const isProtectedRoute = createRouteMatcher([
-//   "/employer(.*)",
-//   "/applicant(.*)",
-//   "/setup(.*)",
-// ]);
-
-// export default clerkMiddleware((auth, req) => {
-//   let authUser = null;
-
-//   if (isProtectedRoute(req)) {
-//     authUser = auth().protect();
-//   }
-
-//   return authorizationMiddleware(req, authUser);
-// });
-
-// export const config = {
-//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
-
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-// import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
   "/employer(.*)",
@@ -31,12 +7,7 @@ const isProtectedRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  // const { pathname } = req.nextUrl;
-  const { userId } = auth();
-  if (isProtectedRoute(req) && !userId) auth().protect();
-  // if (pathname === "/") {
-  //   return NextResponse.redirect(new URL(`/jobs`, req.nextUrl));
-  // }
+  if (isProtectedRoute(req)) auth().protect();
 });
 
 export const config = {
@@ -45,6 +16,5 @@ export const config = {
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
-    "/((?!_next/static|_next/image|favicon.ico).*)",
   ],
 };

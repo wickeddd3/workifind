1124f0f53c1b6e053904eb6ab53a59cbaa1e8433
wickeddd3 <pablophilipandrew@gmail.com>
2025-02-03 23:38:48 +0800

    feat: change JobApplicationForm into server component

--- src/components/jobs/JobApplication/JobApplicationForm.tsx
+++ src/components/jobs/JobApplication/JobApplicationForm.tsx
@@ -1,43 +1,28 @@
-"use client";
-
-import { useCallback, useEffect, useState } from "react";
-import { Applicant, Employer, Job } from "@prisma/client";
-import { useUser } from "@/contexts/UserContext";
-import { getApplicant } from "@/actions/applicants";
 import JobDetails from "@/components/jobs/JobApplication/JobDetails";
 import ApplicantDetails from "@/components/jobs/JobApplication/ApplicantDetails";
 import ApplicationForm from "@/components/jobs/JobApplication/ApplicationForm";
 import LoadingPlaceholder from "@/components/jobs/JobApplication/LoadingPlaceholder";
+import { Applicant, Employer, Job } from "@prisma/client";
+import { Suspense } from "react";
 
 interface JobApplicationFormProps {
+  userId: string;
   job: Job & { employer: Employer };
+  applicant: Applicant;
 }
 
-export default function JobApplicationForm({ job }: JobApplicationFormProps) {
-  const { user } = useUser();
-
-  const [applicant, setApplicant] = useState<Applicant | null>(null);
-
-  const handleGetApplicant = useCallback(
-    async (id: number) => await getApplicant(id),
-    [],
-  );
-
-  useEffect(() => {
-    if (user && user?.id) {
-      handleGetApplicant(user.id).then(setApplicant);
-    }
-  }, [user, handleGetApplicant]);
-
-  return user && job && applicant ? (
-    <section className="flex h-full w-full flex-col gap-4 md:gap-8">
-      {job && <JobDetails job={job} />}
-      {applicant && <ApplicantDetails applicant={applicant} />}
-      {job && user && applicant && (
-        <ApplicationForm job={job} user={user} applicant={applicant} />
-      )}
-    </section>
-  ) : (
-    <LoadingPlaceholder />
+export default function JobApplicationForm({
+  userId,
+  job,
+  applicant,
+}: JobApplicationFormProps) {
+  return (
+    <Suspense fallback={<LoadingPlaceholder />}>
+      <section className="flex h-full w-full flex-col gap-4 md:gap-8">
+        <JobDetails job={job} />
+        <ApplicantDetails applicant={applicant} />
+        <ApplicationForm userId={userId} jobId={job.id} jobSlug={job.slug} />
+      </section>
+    </Suspense>
   );
 }

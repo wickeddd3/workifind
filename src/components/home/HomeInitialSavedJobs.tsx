5ddfbaf657fb6ed62e73f3e54e634983d09c9244
wickeddd3 <pablophilipandrew@gmail.com>
2025-02-03 23:41:46 +0800

    feat: change HomeInitialSavedJobs into server component

--- src/components/home/HomeInitialSavedJobs.tsx
+++ src/components/home/HomeInitialSavedJobs.tsx
@@ -1,66 +1,36 @@
-"use client";
-
-import { getApplicant } from "@/actions/applicants";
-import { getApplicantInitialSavedJobs } from "@/actions/savedJobs";
-import { useUser } from "@/contexts/UserContext";
-import { useCallback, useEffect, useMemo, useState } from "react";
 import SavedJobs from "@/components/home/saved/SavedJobs";
 import SavedJobsEmptyPlaceholder from "@/components/home/saved/SavedJobsEmptyPlaceholder";
-import { Applicant, Employer, Job, SavedJob } from "@prisma/client";
 import SavedJobsUnauthenticated from "@/components/home/saved/SavedJobsUnauthenticated";
 import SavedJobsLoadingPlaceholder from "@/components/home/saved/SavedJobsLoadingPlaceholder";
+import { cache } from "react";
+import { Suspense } from "react";
+import { currentUser } from "@clerk/nextjs/server";
+import { getInitialSavedJobs } from "@/app/_services/applicant-saved-jobs";
 
-export default function HomeInitialSavedJobs() {
-  const { user, userLoading } = useUser();
-  const [applicant, setApplicant] = useState<Applicant | null>(null);
-  const [savedJobs, setSavedJobs] = useState<
-    (SavedJob & { job: Job & { employer: Employer } })[]
-  >([]);
+const handleFetchInitialSavedJobs = cache(async (userId: string) => {
+  return await getInitialSavedJobs(userId);
+});
 
-  const hasJobs = useMemo(() => savedJobs && savedJobs.length > 0, [savedJobs]);
-  const isApplicant = useMemo(() => user && user.role === "APPLICANT", [user]);
+export default async function HomeInitialSavedJobs() {
+  const user = await currentUser();
+  const role = user?.unsafeMetadata?.role;
 
-  const handleGetApplicant = useCallback(
-    async (id: number) => await getApplicant(id),
-    [],
-  );
+  const userId = user?.id;
+  if (!userId) return <SavedJobsUnauthenticated />;
 
-  const handleGetSavedJobs = useCallback(
-    async (id: number) => await getApplicantInitialSavedJobs(id),
-    [],
-  );
-
-  const handleGetApplicantSavedJobs = useCallback(
-    async (userId: number) => {
-      const applicant = await handleGetApplicant(userId);
-      setApplicant(applicant);
-      if (applicant) {
-        const savedJobs = await handleGetSavedJobs(applicant.id);
-        setSavedJobs(savedJobs);
-      }
-    },
-    [handleGetApplicant, handleGetSavedJobs],
-  );
-
-  useEffect(() => {
-    if (user && user?.id) {
-      handleGetApplicantSavedJobs(user.id);
-    }
-  }, [user, handleGetApplicantSavedJobs]);
+  const isApplicant = role === "APPLICANT";
+  const jobs = await handleFetchInitialSavedJobs(userId);
+  const hasJobs = jobs && jobs.length > 0;
 
   return (
     <section className="w-full py-2 md:py-4">
-      {user && isApplicant && applicant && hasJobs && (
-        <SavedJobs savedJobs={savedJobs} />
-      )}
-      {user && isApplicant && applicant && !hasJobs && (
-        <SavedJobsEmptyPlaceholder />
-      )}
-      {user && !isApplicant && (
-        <SavedJobsEmptyPlaceholder message="Only applicant can save jobs" />
-      )}
-      {!user && !userLoading && <SavedJobsUnauthenticated />}
-      {userLoading && <SavedJobsLoadingPlaceholder />}
+      <Suspense fallback={<SavedJobsLoadingPlaceholder />}>
+        {isApplicant && hasJobs && <SavedJobs savedJobs={jobs} />}
+        {isApplicant && !hasJobs && <SavedJobsEmptyPlaceholder />}
+        {!isApplicant && !hasJobs && (
+          <SavedJobsEmptyPlaceholder message="Only applicant can save jobs" />
+        )}
+      </Suspense>
     </section>
   );
 }

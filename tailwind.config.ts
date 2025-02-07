2b634c95e562bddb94020d83e3c182557c0386f9
wickeddd3 <pablophilipandrew@gmail.com>
2025-02-07 21:24:37 +0800

    feat: reinitialize shadcnui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,21 +17,21 @@ const config = {
       xl: "1440px",
     },
     fontSize: {
-      "4xs": ".375rem", // 6px
-      "3xs": ".5rem", // 8px
-      "2xs": ".625rem", // 10px
-      xs: ".75rem", // 12px
-      sm: ".875rem", // 14px
-      md: "1rem", // 16px
-      lg: "1.125rem", // 18px
-      xl: "1.25rem", // 20px
-      "2xl": "1.5rem", // 24px
-      "3xl": "1.875rem", // 30px
-      "4xl": "2.25rem", // 36px
-      "5xl": "3rem", // 48px
-      "6xl": "4rem", // 64px
-      "7xl": "5rem", // 80px
-      "8xl": "6rem", // 96px
+      "4xs": ".375rem",
+      "3xs": ".5rem",
+      "2xs": ".625rem",
+      xs: ".75rem",
+      sm: ".875rem",
+      md: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+      "7xl": "5rem",
+      "8xl": "6rem",
     },
     container: {
       center: true,
@@ -75,6 +75,13 @@ const config = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
       },
       borderRadius: {
         lg: "var(--radius)",
@@ -83,12 +90,20 @@ const config = {
       },
       keyframes: {
         "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
         },
         "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
         },
       },
       animation: {

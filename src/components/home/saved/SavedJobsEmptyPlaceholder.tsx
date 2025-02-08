c36b522f5967cbe92c6ead40d821c5776b99b8ff
wickeddd3 <pablophilipandrew@gmail.com>
2025-02-09 00:33:51 +0800

    feat: set to full width

--- src/components/home/saved/SavedJobsEmptyPlaceholder.tsx
+++ src/components/home/saved/SavedJobsEmptyPlaceholder.tsx
@@ -8,7 +8,7 @@ export default function SavedJobsEmptyPlaceholder({
   message = "Start saving jobs that match your preferences",
 }: SavedJobsEmptyPlaceholderProps) {
   return (
-    <div className="flex flex-col items-center justify-center gap-2 rounded-2xl bg-gray-50 px-4 py-8">
+    <div className="flex w-full flex-col items-center justify-center gap-2 rounded-2xl bg-gray-50 px-4 py-8">
       <IllustrationJobSaved />
       <h4 className="text-lg font-semibold">No saved jobs</h4>
       <p className="text-center text-md font-normal text-muted-foreground">
--- src/components/home/saved/SavedJobsUnauthenticated.tsx
+++ src/components/home/saved/SavedJobsUnauthenticated.tsx
@@ -2,7 +2,7 @@ import IllustrationAuthentication from "@/components/illustrations/IllustrationA
 
 export default function SavedJobsUnauthenticated() {
   return (
-    <div className="flex flex-col items-center justify-center gap-2 rounded-2xl bg-gray-50 px-4 py-8">
+    <div className="flex w-full flex-col items-center justify-center gap-2 rounded-2xl bg-gray-50 px-4 py-8">
       <IllustrationAuthentication />
       <p className="text-center text-md font-normal text-muted-foreground">
         Sign in and start saving jobs that match your preferences

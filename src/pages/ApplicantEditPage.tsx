523844bac5e9c5ba68992f3406e9d0c694fae8e5
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-01 20:21:50 +0800

    refactor: move applicant edit page content to pages layer

---
+++ src/pages/ApplicantEditPage.tsx
@@ -0,0 +1,21 @@
+import ApplicantEditProfile from "@/components/applicant/ApplicantEditProfile";
+import { getApplicantProfileByUserId } from "@/app/_services/applicant";
+import { cache } from "react";
+import { notFound } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
+
+const handleFetchApplicantProfile = cache(async (userId: string) => {
+  return await getApplicantProfileByUserId(userId);
+});
+
+export async function ApplicantEditPage() {
+  const { userId } = auth();
+
+  if (!userId) notFound();
+
+  const applicant = await handleFetchApplicantProfile(userId);
+
+  if (!applicant) notFound();
+
+  return applicant && <ApplicantEditProfile applicant={applicant} />;
+}

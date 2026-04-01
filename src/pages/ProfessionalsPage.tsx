df4b3638403a03c8dacaf52a03c48f15cc389c58
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-01 18:58:08 +0800

    refactor: move professionals page content to pages layer

---
+++ src/pages/ProfessionalsPage.tsx
@@ -0,0 +1,23 @@
+import ProfessionalFilter from "@/components/professionals/ProfessionalFilter";
+import ProfessionalInitialList from "@/components/professionals/ProfessionalInitialList";
+import ProfessionalSearchTip from "@/components/professionals/ProfessionalSearchTip";
+import { cache } from "react";
+import { getInitialProfessionalList } from "@/app/_services/professionals";
+
+const handleGetInitialProfessionalList = cache(async () => {
+  return await getInitialProfessionalList();
+});
+
+export async function ProfessionalsPage() {
+  const professionals = await handleGetInitialProfessionalList();
+
+  return (
+    <main className="m-auto flex max-w-7xl flex-col gap-6 px-3 py-12">
+      <ProfessionalFilter />
+      {professionals && (
+        <ProfessionalInitialList professionals={professionals} />
+      )}
+      <ProfessionalSearchTip />
+    </main>
+  );
+}

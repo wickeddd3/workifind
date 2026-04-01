369bbd0c7135cab9d5901ea97d77856414a318e3
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-01 20:30:14 +0800

    refactor: move applicant saved jobs page content to pages layer

--- src/app/applicant/jobs/saved/page.tsx
+++ src/app/applicant/jobs/saved/page.tsx
@@ -1,53 +1,9 @@
-import ApplicantSavedJobs from "@/components/applicant/ApplicantSavedJobs";
-import ApplicantSavedJobsPagination from "@/components/applicant/ApplicantSavedJobsPagination";
-import ApplicantJobsEmptyPlaceholder from "@/components/applicant/ApplicantJobsEmptyPlaceholder";
-import { notFound } from "next/navigation";
-import {
-  getSavedJobs,
-  getSavedJobsCount,
-} from "@/app/_services/applicant-saved-jobs";
-import { getAuthUser } from "@/shared/lib/clerk";
+import { ApplicantSavedJobsPage } from "@/pages/ApplicantSavedJobsPage";
 
-interface PageProps {
-  searchParams: {
-    page?: string;
-  };
-}
-
-export default async function Page({ searchParams: { page } }: PageProps) {
-  const { user, role } = await getAuthUser();
-  const userId = user?.id;
-  const isApplicant = role === "APPLICANT";
-
-  if (!isApplicant || !userId) return notFound();
-
-  const jobsPerPage = 5;
-  const currentPage = page ? parseInt(page) : 1;
-
-  const [jobs, jobsCount] = await Promise.all([
-    getSavedJobs({ userId, jobsPerPage, page: currentPage }),
-    getSavedJobsCount(userId),
-  ]);
-
-  const hasJobs = jobs && jobs.length > 0;
-
-  return (
-    <main className="m-auto flex flex-col gap-6 px-0 md:px-4">
-      <h1 className="px-4 text-md font-bold md:text-lg">Saved jobs</h1>
-      <div className="flex flex-col gap-2">
-        {hasJobs && (
-          <>
-            <ApplicantSavedJobs savedJobs={jobs} />
-            <ApplicantSavedJobsPagination
-              currentPage={currentPage}
-              totalPages={Math.ceil(jobsCount / jobsPerPage)}
-            />
-          </>
-        )}
-        {!hasJobs && (
-          <ApplicantJobsEmptyPlaceholder message="No saved jobs found" />
-        )}
-      </div>
-    </main>
-  );
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Record<string, string>;
+}) {
+  return <ApplicantSavedJobsPage searchParams={searchParams} />;
 }

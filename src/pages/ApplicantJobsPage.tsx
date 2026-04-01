6bd52fdf6e4b22a7a689bbf65f1b6c39e75f6cb9
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-01 20:27:43 +0800

    refactor: move applicant jobs page content to pages layer

---
+++ src/pages/ApplicantJobsPage.tsx
@@ -0,0 +1,51 @@
+import ApplicantJobApplications from "@/components/applicant/ApplicantJobApplications";
+import ApplicantJobApplicationsPagination from "@/components/applicant/ApplicantJobApplicationsPagination";
+import ApplicantJobsEmptyPlaceholder from "@/components/applicant/ApplicantJobsEmptyPlaceholder";
+import { notFound } from "next/navigation";
+import {
+  getJobApplications,
+  getJobApplicationsCount,
+} from "@/app/_services/applicant-job-applications";
+import { getAuthUser } from "@/shared/lib/clerk";
+
+export async function ApplicantJobsPage({
+  searchParams: { page },
+}: {
+  searchParams: Record<string, string>;
+}) {
+  const { user, role } = await getAuthUser();
+  const userId = user?.id;
+  const isApplicant = role === "APPLICANT";
+
+  if (!isApplicant || !userId) return notFound();
+
+  const jobsPerPage = 5;
+  const currentPage = page ? parseInt(page) : 1;
+
+  const [jobs, jobsCount] = await Promise.all([
+    getJobApplications({ userId, jobsPerPage, page: currentPage }),
+    getJobApplicationsCount(userId),
+  ]);
+
+  const hasJobs = jobs && jobs.length > 0;
+
+  return (
+    <main className="m-auto flex flex-col gap-6 px-0 md:px-4">
+      <h1 className="px-4 text-md font-bold md:text-lg">Applied jobs</h1>
+      <div className="flex flex-col gap-2">
+        {hasJobs && (
+          <>
+            <ApplicantJobApplications jobApplications={jobs} />
+            <ApplicantJobApplicationsPagination
+              currentPage={currentPage}
+              totalPages={Math.ceil(jobsCount / jobsPerPage)}
+            />
+          </>
+        )}
+        {!hasJobs && (
+          <ApplicantJobsEmptyPlaceholder message="No job applications found" />
+        )}
+      </div>
+    </main>
+  );
+}

f106bb9b8ef519ed2962f9fb6566040b9f37202c
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-01 19:06:53 +0800

    refactor: move professional search page content to pages layer

---
+++ src/pages/ProfessionalSearchPage.tsx
@@ -0,0 +1,55 @@
+import ProfessionalSearchFilter from "@/components/professionals/ProfessionalSearchFilter";
+import ProfessionalSearchResults from "@/components/professionals/ProfessionalSearchResults";
+import ProfessionalSearchEmptyPlaceholder from "@/components/professionals/ProfessionalSearchEmptyPlaceholder";
+import ProfessionalSearchResultsPagination from "@/components/professionals/ProfessionalSearchResultsPagination";
+import { ProfessionalFilterSchemaType } from "@/shared/schema/professional-filter";
+import {
+  searchProfessionals,
+  searchProfessionalsCount,
+} from "@/app/_services/professionals";
+
+export async function ProfessionalSearchPage({
+  searchParams: { q, page },
+}: {
+  searchParams: Record<string, string>;
+}) {
+  const filterValues: ProfessionalFilterSchemaType = {
+    q,
+  };
+  const jobsPerPage = 10;
+  const currentPage = page ? parseInt(page) : 1;
+  const query = {
+    searchQuery: q ?? "",
+  };
+  const searchProfessionalsQuery = {
+    ...query,
+    size: jobsPerPage,
+    page: currentPage,
+  };
+
+  const [professionals, totalResults] = await Promise.all([
+    searchProfessionals(searchProfessionalsQuery),
+    searchProfessionalsCount(query),
+  ]);
+
+  const hasProfessionals = professionals && professionals.length > 0;
+
+  return (
+    <main className="m-auto mb-10 flex h-full min-h-screen max-w-2xl flex-col gap-6 px-3">
+      <ProfessionalSearchFilter filterValues={filterValues} />
+      <div className="flex h-full w-full flex-col gap-6">
+        {hasProfessionals && (
+          <>
+            <ProfessionalSearchResults professionals={professionals} />
+            <ProfessionalSearchResultsPagination
+              currentPage={currentPage}
+              totalPages={Math.ceil(totalResults / jobsPerPage)}
+              filterValues={filterValues}
+            />
+          </>
+        )}
+        {!hasProfessionals && <ProfessionalSearchEmptyPlaceholder />}
+      </div>
+    </main>
+  );
+}

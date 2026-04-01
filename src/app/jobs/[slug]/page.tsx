feddc32e4394d7439edd9d8c3cd3cf378c9bff4b
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-01 17:09:51 +0800

    refactor: move job page content to pages layer

--- src/app/jobs/[slug]/page.tsx
+++ src/app/jobs/[slug]/page.tsx
@@ -1,26 +1,9 @@
-import JobDetails from "@/components/jobs/JobDetails";
-import { cache } from "react";
-import { notFound } from "next/navigation";
-import { findJobBySlug } from "@/app/_services/job";
+import { JobPage } from "@/pages/JobPage";
 
-interface PageProps {
+export default async function Page({
+  params: { slug },
+}: {
   params: { slug: string };
-}
-
-const handleFetchJob = cache(async (slug: string) => {
-  return await findJobBySlug(slug);
-});
-
-export default async function Page({ params: { slug } }: PageProps) {
-  const job = await handleFetchJob(slug);
-
-  if (!job) notFound();
-
-  return (
-    <main className="mx-auto max-w-4xl p-4">
-      <div className="h-full">
-        <JobDetails job={job} />
-      </div>
-    </main>
-  );
+}) {
+  return <JobPage slug={slug} />;
 }

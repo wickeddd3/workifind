aaec03c002612f3460c42b56d7e19e0bdfe40f73
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-01 19:21:49 +0800

    refactor: move employer edit page content to pages layer

--- src/app/employer/profile/edit/page.tsx
+++ src/app/employer/profile/edit/page.tsx
@@ -1,21 +1,5 @@
-import EmployerEditProfile from "@/components/employer/EmployerEditProfile";
-import { getEmployerProfileByUserId } from "@/app/_services/employer";
-import { cache } from "react";
-import { notFound } from "next/navigation";
-import { auth } from "@clerk/nextjs/server";
-
-const handleFetchEmployerProfile = cache(async (userId: string) => {
-  return await getEmployerProfileByUserId(userId);
-});
+import { EmployerEditPage } from "@/pages/EmployerEditPage";
 
 export default async function Page() {
-  const { userId } = auth();
-
-  if (!userId) notFound();
-
-  const employer = await handleFetchEmployerProfile(userId);
-
-  if (!employer) notFound();
-
-  return employer && <EmployerEditProfile employer={employer} />;
+  return <EmployerEditPage />;
 }

b018fdf0fac3c5a9d1f6fa16466a1ca82dbb8cd2
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-01 17:16:34 +0800

    refactor: move job application page content to pages layer

---
+++ src/pages/JobApplicationPage.tsx
@@ -0,0 +1,37 @@
+import JobApplicationForm from "@/components/jobs/JobApplication/JobApplicationForm";
+import { cache } from "react";
+import { notFound } from "next/navigation";
+import { findJobBySlug } from "@/app/_services/job";
+import { applyToJobAuthorize } from "@/app/_services/applicant-job-applications";
+import { auth } from "@clerk/nextjs/server";
+import { getApplicantProfileByUserId } from "@/app/_services/applicant";
+
+const handleFetchJob = cache(async (slug: string) => {
+  return await findJobBySlug(slug);
+});
+
+export async function JobApplicationPage({ slug }: { slug: string }) {
+  const { userId } = auth();
+
+  if (!userId) notFound();
+
+  const job = await handleFetchJob(slug);
+
+  if (!job) notFound();
+
+  const isAuthorized = await applyToJobAuthorize(userId, job.id);
+
+  if (!isAuthorized) notFound();
+
+  const applicant = await getApplicantProfileByUserId(userId);
+
+  if (!applicant) notFound();
+
+  return (
+    <main className="mx-auto max-w-4xl p-4">
+      <div className="h-full">
+        <JobApplicationForm userId={userId} job={job} applicant={applicant} />
+      </div>
+    </main>
+  );
+}

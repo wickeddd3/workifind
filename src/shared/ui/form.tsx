a996f4379187f4623eba02d7749b160bbf69c8b1
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-01 16:36:22 +0800

    refactor: move components/ui to shared/ui

--- src/shared/ui/form.tsx
+++ src/shared/ui/form.tsx
@@ -13,7 +13,7 @@ import {
 } from "react-hook-form";
 
 import { cn } from "@/shared/lib/utils";
-import { Label } from "@/components/ui/label";
+import { Label } from "@/shared/ui/label";
 
 const Form = FormProvider;
 

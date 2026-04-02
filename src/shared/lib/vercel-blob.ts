aedd81ec91e7073edaa42c7f43e526adfb888757
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-02 19:07:57 +0800

    feat: add shared/lib vercel-blob

---
+++ src/shared/lib/vercel-blob.ts
@@ -0,0 +1,18 @@
+import { put } from "@vercel/blob";
+
+export async function upload(
+  file: File,
+  filePath: string,
+): Promise<string | null> {
+  try {
+    // Upload file to Vercel Blob Storage
+    const blob = await put(filePath, file, {
+      access: "public", // Set to "private" if you don't want public access
+      contentType: file.type,
+    });
+
+    return blob.url;
+  } catch (error) {
+    return null;
+  }
+}

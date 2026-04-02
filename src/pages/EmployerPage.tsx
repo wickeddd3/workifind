3a079b9c59b9bdf9d56a430ed7282222d8d75dd0
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-02 16:38:21 +0800

    feat: use employer entity to show employer details

--- src/pages/EmployerPage.tsx
+++ src/pages/EmployerPage.tsx
@@ -1,21 +1,30 @@
-import EmployerDetails from "@/components/employer/EmployerDetails";
-import { getEmployerProfileByUserId } from "@/app/_services/employer";
-import { cache } from "react";
 import { notFound } from "next/navigation";
 import { auth } from "@clerk/nextjs/server";
-
-const handleFetchEmployerProfile = cache(async (userId: string) => {
-  return await getEmployerProfileByUserId(userId);
-});
+import {
+  EmployerHeader,
+  EmployerTabs,
+  getEmployerProfile,
+} from "@/entities/employer";
 
 export async function EmployerPage() {
   const { userId } = auth();
 
   if (!userId) notFound();
 
-  const employer = await handleFetchEmployerProfile(userId);
+  const employer = await getEmployerProfile(userId);
 
   if (!employer) notFound();
 
-  return employer && <EmployerDetails employer={employer} />;
+  return (
+    <section className="flex flex-col space-y-6 px-4">
+      <EmployerHeader
+        companyName={employer.companyName}
+        companyEmail={employer.companyEmail}
+        companyWebsite={employer.companyWebsite}
+        companyLogoUrl={employer.companyLogoUrl}
+        hasEditButton={true}
+      />
+      <EmployerTabs employer={employer} />
+    </section>
+  );
 }

fd2143d8942b346b9244063d0866efc3476ce342
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-02 12:53:16 +0800

    feat: use applicant entity to show applicant details

--- src/pages/ApplicantPage.tsx
+++ src/pages/ApplicantPage.tsx
@@ -1,21 +1,38 @@
-import ApplicantDetails from "@/components/applicant/ApplicantDetails";
-import { getApplicantProfileByUserId } from "@/app/_services/applicant";
-import { cache } from "react";
 import { notFound } from "next/navigation";
 import { auth } from "@clerk/nextjs/server";
-
-const handleFetchApplicantProfile = cache(async (userId: string) => {
-  return await getApplicantProfileByUserId(userId);
-});
+import {
+  ApplicantBio,
+  ApplicantHeader,
+  ApplicantLanguages,
+  ApplicantPreferences,
+  ApplicantSkills,
+  getApplicantProfile,
+} from "@/entities/applicant";
 
 export async function ApplicantPage() {
   const { userId } = auth();
 
   if (!userId) notFound();
 
-  const applicant = await handleFetchApplicantProfile(userId);
+  const applicant = await getApplicantProfile(userId);
 
   if (!applicant) notFound();
 
-  return applicant && <ApplicantDetails applicant={applicant} />;
+  return (
+    <section className="flex flex-col space-y-6 px-0 pb-8 md:px-4">
+      <ApplicantHeader applicant={applicant} />
+      <div className="flex flex-col gap-4 px-4 md:px-8">
+        <ApplicantBio bio={applicant.about} />
+        <ApplicantSkills skills={applicant.skills} />
+        <ApplicantLanguages languages={applicant.languages} />
+        <ApplicantPreferences
+          preferredEmploymentTypes={applicant.preferredEmploymentTypes}
+          preferredLocationTypes={applicant.preferredLocationTypes}
+          preferredLocations={applicant.preferredLocations}
+          availability={applicant.availability}
+          salaryExpectation={applicant.salaryExpectation}
+        />
+      </div>
+    </section>
+  );
 }

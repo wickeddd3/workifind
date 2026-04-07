efeff38ebabbe21c07f12dc304b7e47e577c8465
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-07 22:51:30 +0800

    feat: pass ApplyButton component to optionSlot

--- src/pages/JobPage.tsx
+++ src/pages/JobPage.tsx
@@ -1,5 +1,6 @@
 import { notFound } from "next/navigation";
 import { getJobDetailsBySlug, JobDescription, JobHeader } from "@/entities/job";
+import { ApplyButton } from "@/features/job/apply-to-job";
 
 export async function JobPage({ slug }: { slug: string }) {
   const job = await getJobDetailsBySlug(slug);
@@ -8,7 +9,7 @@ export async function JobPage({ slug }: { slug: string }) {
 
   return (
     <section className="mx-auto h-full w-full max-w-4xl grow space-y-5 p-4">
-      <JobHeader job={job} />
+      <JobHeader job={job} optionSlot={<ApplyButton job={job} />} />
       <JobDescription description={job.description} />
     </section>
   );

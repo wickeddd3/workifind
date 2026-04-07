373e84953b846ca104f7d8387d7dbff7f48d5f45
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-08 04:35:45 +0800

    cut: remove console log

--- src/features/job/save-job/api/actions.ts
+++ src/features/job/save-job/api/actions.ts
@@ -13,7 +13,7 @@ export async function authorizeAttempt(
     });
 
     const isAuthorized = !!savedJob;
-    console.log("isAuthorized", isAuthorized);
+
     return isAuthorized;
   } catch (error) {
     return false;

81489be5f3e673f1b51daf426405ebd5f99adb52
wickeddd3 <pablophilipandrew@gmail.com>
2026-04-07 15:56:11 +0800

    feat: add JobApplication type

--- src/entities/job/model/types.ts
+++ src/entities/job/model/types.ts
@@ -1,6 +1,15 @@
-import { Employer, JobApplication, Job as PrismaJob } from "@prisma/client";
+import {
+  Employer,
+  JobApplication as PrismaJobApplication,
+  Job as PrismaJob,
+  Applicant,
+} from "@prisma/client";
 
 export interface Job extends PrismaJob {
   employer: Employer;
   jobApplications?: JobApplication[];
+}
+
+export interface JobApplication extends PrismaJobApplication {
+  applicant: Applicant;
 }
